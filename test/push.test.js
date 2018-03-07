da021fdbc7103c7356152505f0686e2639c6a732
Dom Harrington <dom@harrington-mail.com>
2018-03-07 15:44:15 -0800

    Whoops, fixing up api url and making it work

--- test/push.test.js
+++ test/push.test.js
@@ -10,7 +10,7 @@ describe('push action', () => {
   after(() => nock.cleanAll());
 
   it('should POST to the swagger api if no id provided', (done) => {
-    const mock = nock(config.host.url).post('/v1/api/swagger', (body) => {
+    const mock = nock(config.host.url).post('/api/v1/swagger', (body) => {
       return body.match('form-data; name=\"swagger\"');
     }).basicAuth({ user: apiKey }).reply(201);
 
@@ -24,7 +24,7 @@ describe('push action', () => {
 
   it('should PUT to the swagger api if id is provided', (done) => {
     const id = '5aa0409b7cf527a93bfb44df';
-    const mock = nock(config.host.url).put(`/v1/api/swagger/${id}`, (body) => {
+    const mock = nock(config.host.url).put(`/api/v1/swagger/${id}`, (body) => {
       return body.match('form-data; name=\"swagger\"');
     }).basicAuth({ user: apiKey }).reply(201);
 
