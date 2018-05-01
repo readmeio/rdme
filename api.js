b93c9ad66a9b810e727f44f6fb20d9355d1046c2
Dom Harrington <dom@harrington-mail.com>
2018-05-01 14:10:30 -0700

    Remove unused lib files

--- api.js
+++ api.js
@@ -22,21 +22,15 @@ exports.api = function(args, opts = {}) {
   actionObj.run(config, info);
 };
 
-exports.load = function(config, action = 'start') {
+exports.load = function(config, action = 'help') {
   const file = path.join(__dirname, 'lib', `${action}.js`);
-  if (utils.fileExists(file)) {
+  try {
     // eslint-disable-next-line global-require, import/no-dynamic-require
     return require(file);
+  } catch (e) {
+    console.error('Action not found.'.red);
+    console.warn(`Type ${`${config.cli} help`.yellow} to see all commands`);
+    process.exitCode = 1;
+    return undefined;
   }
-
-  const alias = utils.getAliasFile(action);
-  if (alias) {
-    // eslint-disable-next-line global-require, import/no-dynamic-require
-    return require(path.join(__dirname, 'lib', `${alias}.js`));
-  }
-
-  console.error('Action not found.'.red);
-  console.warn(`Type ${`${config.cli} help`.yellow} to see all commands`);
-  process.exitCode = 1;
-  return undefined;
 };
--- test/utils.test.js
+++ test/utils.test.js
@@ -1,38 +1,9 @@
 const assert = require('assert');
-const path = require('path');
 
 const utils = require('../utils');
 
-describe('utils.js', () => {
-  describe('#findSwagger()', () => {
-    it.skip('find a YAML file', done => {
-      utils.findSwagger(
-        (err, swagger, file) => {
-          if (err) return done(err);
-          assert(file.endsWith('PetStore.yaml'));
-          assert.equal('2.0', swagger.swagger);
-          return done();
-        },
-        {
-          dir: path.join(__dirname, 'fixtures', 'yaml'),
-        },
-      );
-    });
-
-    it.skip('find a JSON file', done => {
-      utils.findSwagger(
-        (err, swagger, file) => {
-          if (err) return done(err);
-          assert(file.endsWith('swagger.json'));
-          assert.equal('2.0', swagger.swagger);
-          return done();
-        },
-        {
-          dir: path.join(__dirname, 'fixtures', 'json'),
-        },
-      );
-    });
-
+describe('utils', () => {
+  describe('#config()', () => {
     it('loads main config', () => {
       const config = utils.config('config');
       assert(Object.keys(config).length > 0);
