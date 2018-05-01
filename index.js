810c7ee7f6d040dab1038a9f9e4ac3cf1d94037b
Dom Harrington <dom@harrington-mail.com>
2018-05-01 14:42:53 -0700

    Remove unused packages and simplify a few things

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 #! /usr/bin/env node
-const _ = require('lodash');
-
 const parseArgs = require('minimist')(process.argv.slice(2));
 
-const args = parseArgs._;
-const opts = _.clone(parseArgs);
-delete opts._;
-
-require('./api').api(args, opts);
+require('./api').api(parseArgs._, parseArgs);
