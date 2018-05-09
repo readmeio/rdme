173ddb9bfc928bb4f78a86c86cc29f20ed648709
Dom Harrington <dom@harrington-mail.com>
2018-05-09 12:03:51 -0700

    Tidying up how validation errors are logged from the API

--- rdme.js
+++ rdme.js
@@ -16,7 +16,13 @@ const parseArgs = require('minimist')(process.argv.slice(2), {
 require('./cli')(parseArgs._[0], parseArgs._.slice(1), parseArgs)
   .then(() => process.exit())
   .catch(err => {
-    if (err) console.error(err.message.red);
-    if (err.description) console.warn(err.description);
+    if (err) {
+      // `err.message` is from locally thrown Error objects
+      // `err.error` is from remote API errors
+      console.error((err.message || err.error).red);
+      if (err.description) console.warn(err.description);
+      if (err.errors) console.warn(err.errors);
+    }
+
     return process.exit(1);
   });
