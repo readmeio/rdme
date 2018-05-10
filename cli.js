633b64bb6a6d996e6da6cb1a1c2618d190f9c86e
Dom Harrington <dom@harrington-mail.com>
2018-05-09 17:14:02 -0700

    Fix issue with loading `config` from a node_module

--- cli.js
+++ cli.js
@@ -1,5 +1,18 @@
 const path = require('path');
+// We have to do this otherwise `require('config')` loads
+// from the cwd where the user is running `rdme` which
+// wont be what we want
+//
+// This is a little sketchy overwriting environment variables
+// but since this is only supposed to be a cli and not
+// requireable, i think this is okay
+const configDir = process.env.NODE_CONFIG_DIR;
+process.env.NODE_CONFIG_DIR = path.join(__dirname, '/config');
+
 const config = require('config');
+
+process.env.NODE_CONFIG_DIR = configDir;
+
 const { version } = require('./package.json');
 
 function load(command = 'help') {
