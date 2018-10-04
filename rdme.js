da1ba62c69cb49dd8f6b47b1c5e05c8f9ee83dcb
Dom Harrington <dom@harrington-mail.com>
2018-10-04 16:37:03 -0700

    Prettier

--- rdme.js
+++ rdme.js
@@ -22,7 +22,7 @@ require('./cli')(parseArgs._[0] || 'help', parseArgs._.slice(1), parseArgs)
     if (err) {
       // `err.message` is from locally thrown Error objects
       // `err.error` is from remote API errors
-      if (err.message) console.error(err.message.red)
+      if (err.message) console.error(err.message.red);
       if (err.description) console.warn(err.description);
       if (err.errors) console.warn(err.errors);
     }
