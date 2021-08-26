7e8d4a6d10e92b4ede90128d116345a01f755832
Kanad Gupta <kgupta@umn.edu>
2021-08-26 17:40:23 -0500

    chore: update default host URL

--- config/default.js
+++ config/default.js
@@ -1,6 +1,6 @@
 module.exports = {
   // eslint-disable-next-line global-require
   cli: require('../package.json').name,
-  host: 'https://dash.readme.io',
+  host: 'https://dash.readme.com',
   hub: 'https://{project}.readme.io',
 };
