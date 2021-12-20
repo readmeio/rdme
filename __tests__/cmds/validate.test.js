02f5cd49c2bd9bd52f7604cf97f43da168d378cd
Kanad Gupta <kgupta@umn.edu>
2021-12-20 12:51:56 -0800

    test: check for resolved output, rather than console output

--- __tests__/cmds/validate.test.js
+++ __tests__/cmds/validate.test.js
@@ -1,19 +1,18 @@
 const fs = require('fs');
+const chalk = require('chalk');
 const validate = require('../../src/cmds/validate');
 
 const getCommandOutput = () => {
-  return [console.warn.mock.calls.join('\n\n'), console.log.mock.calls.join('\n\n')].filter(Boolean).join('\n\n');
+  return [console.info.mock.calls.join('\n\n')].filter(Boolean).join('\n\n');
 };
 
 describe('rdme validate', () => {
   beforeEach(() => {
-    console.log = jest.fn();
-    console.warn = jest.fn();
+    console.info = jest.fn();
   });
 
   afterEach(() => {
-    console.log.mockRestore();
-    console.warn.mockRestore();
+    console.info.mockRestore();
   });
 
   it.each([
@@ -24,37 +23,30 @@ describe('rdme validate', () => {
     ['OpenAPI 3.1', 'json', '3.1'],
     ['OpenAPI 3.1', 'yaml', '3.1'],
   ])('should support validating a %s definition (format: %s)', (_, format, specVersion) => {
-    return validate
-      .run({
+    expect(console.info).toHaveBeenCalledTimes(0);
+    return expect(
+      validate.run({
         spec: require.resolve(`@readme/oas-examples/${specVersion}/${format}/petstore.${format}`),
       })
-      .then(() => {
-        expect(console.log).toHaveBeenCalledTimes(1);
-
-        const output = getCommandOutput();
-        if (specVersion === '2.0') {
-          expect(output).toContain(`petstore.${format} is a valid Swagger API definition!`);
-        } else {
-          expect(output).toContain(`petstore.${format} is a valid OpenAPI API definition!`);
-        }
-      });
+    ).resolves.toContain(
+      `petstore.${format} is a valid ${specVersion === '2.0' ? 'Swagger' : 'OpenAPI'} API definition!`
+    );
   });
 
-  it('should discover and upload an API definition if none is provided', () => {
+  it('should discover and upload an API definition if none is provided', async () => {
     // Surface our test fixture to the root directory so rdme can autodiscover it. It's easier to do
     // this than mocking out the fs module because mocking the fs module here causes Jest sourcemaps
     // to break.
-    fs.copyFileSync(require.resolve('@readme/oas-examples/2.0/json/petstore.json'), './swagger.json');
+    await fs.copyFileSync(require.resolve('@readme/oas-examples/2.0/json/petstore.json'), './swagger.json');
 
-    return validate.run({}).then(() => {
-      expect(console.log).toHaveBeenCalledTimes(2);
+    await expect(validate.run({})).resolves.toBe(chalk.green('swagger.json is a valid Swagger API definition!'));
 
-      const output = getCommandOutput();
-      expect(output).toContain('We found swagger.json and are attempting to validate it.');
-      expect(output).toContain('swagger.json is a valid Swagger API definition!');
+    expect(console.info).toHaveBeenCalledTimes(1);
 
-      fs.unlinkSync('./swagger.json');
-    });
+    const output = getCommandOutput();
+    expect(output).toBe(chalk.yellow('We found swagger.json and are attempting to validate it.'));
+
+    return fs.unlinkSync('./swagger.json');
   });
 
   describe('error handling', () => {
