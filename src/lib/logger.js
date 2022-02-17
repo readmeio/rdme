a6f1373ee9a9ad0d590f7e607e61e77bacad61f7
Kanad Gupta <kgupta@umn.edu>
2022-02-17 16:36:42 -0600

    revert: skip warn function for now

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -13,17 +13,5 @@ function debug(arg) {
   return debugPackage(arg);
 }
 
-/**
- * Logs the arguments to stdout using console.warn()
- * with yellow text and prefixed with a '⚠️  Warning!'
- * @param  {...any} args Any number of arguments.
- * The output will return the arguments separated
- * by a space.
- */
-module.exports.warn = function (...args) {
-  if (isGHA() && process.env.NODE_ENV !== 'testing') return core.warning(args.join(' '));
-  return console.warn(chalk.yellow('⚠️  Warning!', ...args));
-};
-
 module.exports = debug;
 module.exports.debug = debug;
