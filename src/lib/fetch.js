9605aca2bb7d953ae557e41d1b3c911797223a96
Kanad Gupta <kgupta@umn.edu>
2022-02-23 14:30:08 -0600

    fix: specify method in fetch debug

--- src/lib/fetch.js
+++ src/lib/fetch.js
@@ -25,7 +25,7 @@ module.exports = (url, options = { headers: {} }) => {
 
   options.headers['x-readme-source'] = source;
 
-  debug(`making fetch request to ${url}`);
+  debug(`making ${options.method.toUpperCase() || 'GET'} request to ${url}`);
 
   return fetch(url, options);
 };
