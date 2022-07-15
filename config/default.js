122ba5c942639489bf51e97e6dbf9444e40cf154
Kanad Gupta <kgupta@umn.edu>
2022-07-15 09:05:51 -0500

    feat(config): add ability to override host + hub (#535)

--- config/default.js
+++ config/default.js
@@ -2,5 +2,5 @@ module.exports = {
   // eslint-disable-next-line global-require
   cli: require('../package.json').name,
   host: 'https://dash.readme.com',
-  hub: 'https://{project}.readme.io',
+  hub: 'https://{project}.readme.io', // this is only used for the `open` command
 };
