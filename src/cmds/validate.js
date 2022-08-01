33f7ec31ff61555fe0c3cfa5eb4b6ca9cc23eef2
Kanad Gupta <kgupta@umn.edu>
2022-08-01 14:07:28 -0400

    refactor(openapi): DRY and reorg

--- src/cmds/validate.js
+++ src/cmds/validate.js
@@ -1,5 +1,4 @@
 const chalk = require('chalk');
-const fs = require('fs');
 const { debug } = require('../lib/logger');
 const prepareOas = require('../lib/prepareOas');
 
@@ -36,34 +35,7 @@ module.exports = class ValidateCommand {
     debug(`command: ${this.command}`);
     debug(`opts: ${JSON.stringify(opts)}`);
 
-    async function validateSpec(specPath) {
-      const { specType } = await prepareOas(specPath);
-      return Promise.resolve(chalk.green(`${specPath} is a valid ${specType} API definition!`));
-    }
-
-    if (spec) {
-      return validateSpec(spec);
-    }
-
-    // If the user didn't supply an API specification, let's try to locate what they've got, and validate that. If they
-    // don't have any, let's let the user know how they can get one going.
-    return new Promise((resolve, reject) => {
-      ['swagger.json', 'swagger.yaml', 'swagger.yml', 'openapi.json', 'openapi.yaml', 'openapi.yml'].forEach(file => {
-        debug(`looking for definition with filename: ${file}`);
-        if (!fs.existsSync(file)) {
-          debug(`${file} not found`);
-          return;
-        }
-
-        console.info(chalk.yellow(`We found ${file} and are attempting to validate it.`));
-        resolve(validateSpec(file));
-      });
-
-      reject(
-        new Error(
-          "We couldn't find an OpenAPI or Swagger definition.\n\nPlease specify the path to your definition with `rdme validate ./path/to/api/definition`."
-        )
-      );
-    });
+    const { specPath, specType } = await prepareOas(spec, this.command);
+    return Promise.resolve(chalk.green(`${specPath} is a valid ${specType} API definition!`));
   }
 };
