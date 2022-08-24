6beb84a7f48291c8990c186800bb7659c9d5acdb
Kanad Gupta <kgupta@umn.edu>
2022-08-24 13:51:37 -0500

    chore: clean up unused interfaces
    i really don't understand what's going on here lol

--- __tests__/helpers/jest.matchers.ts
+++ __tests__/helpers/jest.matchers.ts
@@ -15,11 +15,7 @@ declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace jest {
     // eslint-disable-next-line @typescript-eslint/no-empty-interface
-    interface Expect extends CustomMatchers {}
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
     interface Matchers<R> extends CustomMatchers<R> {}
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
-    interface InverseAsymmetricMatchers extends CustomMatchers {}
   }
 }
 
