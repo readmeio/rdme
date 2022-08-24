e659b3706a1a2f064efa0ab74d8b46b2a2850295
Kanad Gupta <kgupta@umn.edu>
2022-08-24 17:08:18 -0500

    refactor: move helper function to separate file

--- __tests__/lib/createGHA.test.ts
+++ __tests__/lib/createGHA.test.ts
@@ -13,51 +13,13 @@ import prompts from 'prompts';
 import OpenAPICommand from '../../src/cmds/openapi';
 import ValidateCommand from '../../src/cmds/validate';
 import createGHA, { cleanUpFileName, git, getGitData } from '../../src/lib/createGHA';
+import createGitRemoteMock from '../helpers/get-git-mock';
 import ghaWorkflowSchemaBackup from '../helpers/github-workflow-schema.json';
 
 const ghaWorkflowUrl = 'https://json.schemastore.org/github-workflow.json';
 
 let consoleInfoSpy;
 const getCommandOutput = () => consoleInfoSpy.mock.calls.join('\n\n');
-
-/**
- * Creates a Jest mock function for testing `git.remote`
- * @param remote remote to return (usually `origin`)
- * @param remoteUrl git URL for the given remote
- * @param defaultBranch the HEAD branch
- */
-function createGitRemoteMock(
-  remote = 'origin',
-  remoteUrl = 'https://github.com/readmeio/rdme.git',
-  defaultBranch = 'main'
-) {
-  return jest.fn(arr => {
-    // first call (used to grab remote for usage in subsequent commands)
-    if (!arr.length) {
-      if (!remote) return Promise.reject(new Error('Bad mock uh oh')) as unknown as Response<string>;
-      return Promise.resolve(remote) as unknown as Response<string>;
-    }
-    // second call (used to grab default branch)
-    if (arr.length === 2 && arr[0] === 'show' && arr[1] === remote) {
-      if (!defaultBranch) return Promise.reject(new Error('Bad mock uh oh')) as unknown as Response<string>;
-      return Promise.resolve(`* remote origin
-  Fetch URL: ${remoteUrl}
-  Push  URL: ${remoteUrl}
-  HEAD branch: ${defaultBranch}
-`) as unknown as Response<string>;
-    }
-
-    // third call (used to grab remote URLs)
-    if (arr.length === 1 && arr[0] === '-v') {
-      if (!remoteUrl) return Promise.reject(new Error('Bad mock uh oh')) as unknown as Response<string>;
-      return Promise.resolve(`origin  ${remoteUrl} (fetch)
-origin  ${remoteUrl} (push)
-    `) as unknown as Response<string>;
-    }
-
-    return Promise.reject(new Error('Bad mock uh oh')) as unknown as Response<string>;
-  });
-}
 
 describe('#createGHA', () => {
   let ghaWorkflowSchema;
