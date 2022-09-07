b0ebb5a570111da8aba7f21dde92e792f6703d28
Kanad Gupta <kgupta@umn.edu>
2022-09-07 11:33:53 -0500

    fix: don't document `validate` command

--- src/cmds/validate.ts
+++ src/cmds/validate.ts
@@ -1,8 +1,11 @@
 import type { CommandOptions } from '../lib/baseCommand';
 import type { Options } from './openapi/validate';
 
+import isHidden from '../lib/decorators/isHidden';
+
 import OpenAPIValidateCommand from './openapi/validate';
 
+@isHidden
 export default class ValidateAliasCommand extends OpenAPIValidateCommand {
   constructor() {
     super();
