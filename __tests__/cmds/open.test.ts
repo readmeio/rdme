1288a6915ccd5ac2d90f4cece5d3d5ad959c673b
Darren Yong <contact.darrenyong@gmail.com>
2022-11-16 15:51:26 -0800

    test: add tests for `rdme open` with `--dash` flag

--- __tests__/cmds/open.test.ts
+++ __tests__/cmds/open.test.ts
@@ -1,8 +1,11 @@
+import type { Version } from '../../src/cmds/versions';
+
 import chalk from 'chalk';
 import config from 'config';
 
 import Command from '../../src/cmds/open';
 import configStore from '../../src/lib/configstore';
+import getAPIMock from '../helpers/get-api-mock';
 
 const cmd = new Command();
 
@@ -12,6 +15,7 @@ describe('rdme open', () => {
   });
 
   it('should error if no project provided', () => {
+    configStore.delete('project');
     configStore.delete('project');
 
     return expect(cmd.run({})).rejects.toThrow(`Please login using \`${config.get('cli')} login\`.`);
@@ -29,5 +33,37 @@ describe('rdme open', () => {
     }
 
     return expect(cmd.run({ mockOpen })).resolves.toBe(`Opening ${chalk.green(projectUrl)} in your browser...`);
+  });
+
+  it('should open the dash', () => {
+    expect.assertions(2);
+    configStore.set('project', 'subdomain');
+    configStore.set('apiKey', '12345');
+
+    const version = '1.0';
+    const key = '12345';
+    const versionPayload: Version = {
+      createdAt: '2019-06-17T22:39:56.462Z',
+      is_deprecated: false,
+      is_hidden: false,
+      is_beta: false,
+      is_stable: true,
+      codename: '',
+      version,
+    };
+
+    const mockRequest = getAPIMock().get('/api/v1/version').basicAuth({ user: key }).reply(200, [versionPayload]);
+
+    const dashUrl = 'https://dash.readme.com/project/subdomain/v1.0/overview';
+
+    function mockOpen(url: string) {
+      expect(url).toBe(dashUrl);
+      return Promise.resolve();
+    }
+
+    return expect(cmd.run({ mockOpen, dash: true, key: '12345' })).resolves.toBe(
+      `Opening ${chalk.green(dashUrl)} in your browser...`
+    );
+    mockRequest.done();
   });
 });
