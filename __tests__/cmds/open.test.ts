40c5936a5309cb71362c39878999a83d975da131
Darren Yong <contact.darrenyong@gmail.com>
2022-11-17 10:33:10 -0800

    test: update `open --dash` test

--- __tests__/cmds/open.test.ts
+++ __tests__/cmds/open.test.ts
@@ -16,7 +16,6 @@ describe('rdme open', () => {
 
   it('should error if no project provided', () => {
     configStore.delete('project');
-    configStore.delete('project');
 
     return expect(cmd.run({})).rejects.toThrow(`Please login using \`${config.get('cli')} login\`.`);
   });
@@ -35,35 +34,52 @@ describe('rdme open', () => {
     return expect(cmd.run({ mockOpen })).resolves.toBe(`Opening ${chalk.green(projectUrl)} in your browser...`);
   });
 
-  it('should open the dash', () => {
-    expect.assertions(2);
-    configStore.set('project', 'subdomain');
-    configStore.set('apiKey', '12345');
+  describe('open --dash', () => {
+    it('should open the dash', () => {
+      expect.assertions(2);
+      configStore.set('project', 'subdomain');
+      configStore.set('apiKey', '12345');
 
-    const version = '1.0';
-    const key = '12345';
-    const versionPayload: Version = {
-      createdAt: '2019-06-17T22:39:56.462Z',
-      is_deprecated: false,
-      is_hidden: false,
-      is_beta: false,
-      is_stable: true,
-      codename: '',
-      version,
-    };
+      const version = '1.0';
+      const key = '12345';
+      const versionPayload: Version = {
+        createdAt: '2019-06-17T22:39:56.462Z',
+        is_deprecated: false,
+        is_hidden: false,
+        is_beta: false,
+        is_stable: true,
+        codename: '',
+        version,
+      };
 
-    const mockRequest = getAPIMock().get('/api/v1/version').basicAuth({ user: key }).reply(200, [versionPayload]);
+      const mockRequest = getAPIMock().get('/api/v1/version').basicAuth({ user: key }).reply(200, [versionPayload]);
 
-    const dashUrl = 'https://dash.readme.com/project/subdomain/v1.0/overview';
+      const dashUrl = 'https://dash.readme.com/project/subdomain/v1.0/overview';
 
-    function mockOpen(url: string) {
-      expect(url).toBe(dashUrl);
-      return Promise.resolve();
-    }
+      function mockOpen(url: string) {
+        expect(url).toBe(dashUrl);
+        return Promise.resolve();
+      }
 
-    return expect(cmd.run({ mockOpen, dash: true, key: '12345' })).resolves.toBe(
-      `Opening ${chalk.green(dashUrl)} in your browser...`
-    );
-    mockRequest.done();
+      return expect(cmd.run({ mockOpen, dash: true, key: '12345' })).resolves.toBe(
+        `Opening ${chalk.green(dashUrl)} in your browser...`
+      );
+      mockRequest.done();
+    });
+
+    it('should require user to be logged in', () => {
+      configStore.set('project', 'subdomain');
+
+      const dashUrl = 'https://dash.readme.com/project/subdomain/v1.0/overview';
+
+      function mockOpen(url: string) {
+        expect(url).toBe(dashUrl);
+        return Promise.resolve();
+      }
+
+      return expect(cmd.run({ mockOpen, dash: true, key: '12345' })).rejects.toStrictEqual(
+        new Error(`Please login using \`${config.get('cli')} login\`.`)
+      );
+    });
   });
 });
