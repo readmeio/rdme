ec462ad1a4fe2fe2c1c4437683069b8e6c506f46
Kanad Gupta <8854718+kanadgupta@users.noreply.github.com>
2022-11-29 10:39:35 -0600

    feat: surface major version tag in our docs

--- bin/set-version-output.js
+++ bin/set-version-output.js
@@ -1,14 +1,15 @@
 #! /usr/bin/env node
 const core = require('@actions/core');
 
-const { getNodeVersion, getPkgVersion } = require('../dist/src/lib/getPkgVersion');
+const { getNodeVersion, getMajorPkgVersion } = require('../dist/src/lib/getPkgVersion');
 
 /**
- * Sets output parameters for GitHub Actions workflow
+ * Sets output parameters for GitHub Actions workflow so we can do
+ * a find-and-replace in our docs prior to syncing them to ReadMe
  * Docs: https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter
  */
 async function setOutputs() {
-  core.setOutput('RDME_VERSION', await getPkgVersion('latest'));
+  core.setOutput('RDME_VERSION', `v${await getMajorPkgVersion('latest')}`);
   core.setOutput('NODE_VERSION', getNodeVersion());
 }
 
