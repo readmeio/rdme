91b216e266d739ba84258e39e2877835bccd3a34
Kanad Gupta <8854718+kanadgupta@users.noreply.github.com>
2022-11-28 18:11:24 -0600

    refactor: use core function for output setting
    This pattern for setting outputs is now deprecated

--- bin/set-version-output.js
+++ bin/set-version-output.js
@@ -1,17 +1,16 @@
 #! /usr/bin/env node
+const core = require('@actions/core');
+
 const { getNodeVersion } = require('../dist/src/lib/getNodeVersion');
 const { getPkgVersion } = require('../dist/src/lib/getPkgVersion');
-
-const name1 = 'RDME_VERSION';
-const name2 = 'NODE_VERSION';
 
 /**
  * Sets output parameters for GitHub Actions workflow
  * Docs: https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter
  */
 async function setOutputs() {
-  console.log(`::set-output name=${name1}::${await getPkgVersion('latest')}`); // eslint-disable-line no-console
-  console.log(`::set-output name=${name2}::${getNodeVersion()}`); // eslint-disable-line no-console
+  core.setOutput('RDME_VERSION', await getPkgVersion('latest'));
+  core.setOutput('NODE_VERSION', getNodeVersion());
 }
 
 setOutputs();
