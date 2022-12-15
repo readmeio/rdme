a640f622d1e1fb1a899623347029dc1572e2f52b
Kanad Gupta <git@kanad.dev>
2022-12-15 17:07:57 -0600

    docs: JSDoc

--- src/lib/loginFlow.ts
+++ src/lib/loginFlow.ts
@@ -27,6 +27,8 @@ function loginFetch(body: LoginBody) {
  * The prompt flow for logging a user in and writing the credentials to
  * `configstore`. This is a separate lib function because we reuse it both
  * in the `login` command as well as any time a user omits an API key.
+ * @param otp an optional one-time passcode, if the user passes one in
+ * via a flag to the the `login` command
  * @returns A Promise-wrapped string with the logged-in user's credentials
  */
 export default async function loginFlow(otp?: string) {
