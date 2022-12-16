9561f7c26bdcd0ad4e1f80a975db95dff27b9670
Kanad Gupta <8854718+kanadgupta@users.noreply.github.com>
2022-12-16 15:58:52 -0600

    fix: login issue when user doesn't pass in API key (#714)

--- src/lib/baseCommand.ts
+++ src/lib/baseCommand.ts
@@ -89,7 +89,7 @@ export default class Command {
     Command.debug(`opts: ${JSON.stringify(opts)}`);
 
     if (this.args.some(arg => arg.name === 'key')) {
-      const { apiKey, email, project } = getCurrentConfig();
+      const { email, project } = getCurrentConfig();
 
       // We only want to log this if the API key is stored in the configstore, **not** in an env var.
       if (opts.key && configstore.get('apiKey') === opts.key) {
@@ -109,7 +109,7 @@ export default class Command {
         const result = await loginFlow();
         info(result, { includeEmojiPrefix: false });
         // eslint-disable-next-line no-param-reassign
-        opts.key = apiKey;
+        opts.key = configstore.get('apiKey');
       }
     }
 
