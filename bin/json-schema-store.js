b17f518f64b1ff01c48f4ac173a01ed536a0168d
Kanad Gupta <git@kanad.dev>
2023-02-14 18:39:36 -0500

    ci: add check for json schema store

---
+++ bin/json-schema-store.js
@@ -0,0 +1,55 @@
+#! /usr/bin/env node
+/* eslint-disable import/no-extraneous-dependencies */
+const fs = require('fs/promises');
+
+const prettierConfig = require('@readme/eslint-config/prettier');
+const fetch = require('node-fetch');
+const prettier = require('prettier');
+
+const files = [
+  {
+    // JSON Schema for GitHub Actions Workflow file
+    // See: https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions
+    filePath: '__tests__/helpers/github-workflow-schema.json',
+    url: 'https://raw.githubusercontent.com/SchemaStore/schemastore/master/src/schemas/json/github-workflow.json',
+  },
+];
+
+/**
+ * Handles the checks and refreshes for our JSON Schema Store files.
+ *
+ * @see {@link https://www.schemastore.org/json/}
+ */
+async function refreshSchemas() {
+  const isUpdate = process.argv.includes('--update');
+  try {
+    await Promise.all(
+      files.map(async file => {
+        const rawFile = await fetch(file.url, {}).then(res => {
+          if (!res.ok) throw new Error(`Received ${res.status} when attempting to fetch ${file.url}`);
+          return res.json();
+        });
+
+        const formattedFile = prettier.format(JSON.stringify(rawFile), { parser: 'json', ...prettierConfig });
+
+        if (isUpdate) {
+          await fs.writeFile(file.filePath, formattedFile);
+        } else {
+          const currentFile = await fs.readFile(file.filePath, { encoding: 'utf-8' });
+          if (currentFile !== formattedFile) {
+            throw new Error(`${file.filePath} is outdated! Run \`npm run schemas:write\` to update.`);
+          }
+        }
+      })
+    );
+    // eslint-disable-next-line no-console
+    console.log(isUpdate ? 'JSON Schema files updated!' : 'JSON Schema files are up-to-date!');
+    process.exit(0);
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e.message);
+    process.exit(1);
+  }
+}
+
+refreshSchemas();
