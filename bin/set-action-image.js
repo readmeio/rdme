d396f5eff0d09095cd2822ea0bc504fa07f17797
Kanad Gupta <git@kanad.dev>
2023-03-07 10:53:08 -0600

    fix: new approach to obtaining next version

--- bin/set-action-image.js
+++ bin/set-action-image.js
@@ -1,16 +1,13 @@
 #! /usr/bin/env node
 const fs = require('fs/promises');
 
-const core = require('@actions/core');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const jsYaml = require('js-yaml');
 
 /**
  * Updates our `action.yml` file so it properly points to
- * the correct docker image, and also sets a version output
- * for usage in subsequent workflow Docker image building steps
+ * the correct docker image
  * @see {@link https://docs.github.com/en/actions/creating-actions/metadata-syntax-for-github-actions#runs-for-docker-container-actions}
- * @see {@link https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter}
  */
 async function setActionImage() {
   // Grab version from args
@@ -20,8 +17,6 @@ async function setActionImage() {
     console.error('Missing version in setActionImage()');
     process.exit(1);
   }
-  // Set GitHub Output
-  core.setOutput('newVersion', newVersion);
   // Grabs Docker image URL from action.yml, updates version value,
   // and writes changes back to action.yml file
   const actionFile = await fs.readFile('./action.yml', 'utf-8');
