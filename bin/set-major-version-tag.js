79ae0d65ac05c9cdfb3af5fe222fcd1902dea08b
Kanad Gupta <git@kanad.dev>
2023-03-29 14:53:31 -0700

    ci: fix major version tagging process

--- bin/set-major-version-tag.js
+++ bin/set-major-version-tag.js
@@ -1,9 +1,31 @@
 #! /usr/bin/env node
-const { exec } = require('child_process');
+const util = require('util'); // eslint-disable-line unicorn/import-style
+const execFile = util.promisify(require('child_process').execFile);
 
 const semverParse = require('semver/functions/parse');
 
 const pkg = require('../package.json');
+
+/**
+ * Runs command and logs all output
+ */
+async function runGitCmd(args) {
+  // Promise-based approach grabbed from here: https://stackoverflow.com/a/63027900
+  const execCmd = execFile('git', args);
+  const child = execCmd.child;
+
+  child.stdout.on('data', chunk => {
+    // eslint-disable-next-line no-console
+    console.log(chunk.toString());
+  });
+
+  child.stderr.on('data', chunk => {
+    // eslint-disable-next-line no-console
+    console.error(chunk.toString());
+  });
+
+  await execCmd;
+}
 
 /**
  * Sets major git tag as part of release process
@@ -11,23 +33,36 @@ const pkg = require('../package.json');
  * @example v7
  */
 async function setMajorVersionTag() {
-  const parsedVersion = semverParse(pkg.version);
+  try {
+    const parsedVersion = semverParse(pkg.version);
 
-  if (parsedVersion.prerelease.length) {
-    // eslint-disable-next-line no-console
-    console.warn('Pre-release version, not setting major version tag');
-    return;
-  }
-
-  exec(`git tag v${parsedVersion.major} -f -m 'Top-level tag pointing to ${parsedVersion.version}'`, (err, stdout) => {
-    if (err) {
+    if (parsedVersion.prerelease.length) {
       // eslint-disable-next-line no-console
-      console.error(err);
-      process.exit(1);
+      console.warn('Pre-release version, not setting major version tag');
+      return;
     }
+
+    const majorTag = `v${parsedVersion.major}`;
+
+    await runGitCmd(['tag', majorTag, '--force', '--message', `Top-level tag pointing to ${parsedVersion.version}`]);
+
+    const args = process.argv.slice(2);
+
+    if (args[0] === 'push') {
+      await runGitCmd(['push', 'origin', majorTag, '--force']);
+      // eslint-disable-next-line no-console
+      console.log(`🏷️  Created and pushed ${majorTag}`);
+    } else {
+      // eslint-disable-next-line no-console
+      console.log("Not pushing, missing 'push' argument");
+    }
+  } catch (e) {
     // eslint-disable-next-line no-console
-    if (stdout) console.log(stdout);
-  });
+    console.error('Error running git major version tagging script!');
+    // eslint-disable-next-line no-console
+    console.error(e);
+    process.exit(1);
+  }
 }
 
 setMajorVersionTag();
