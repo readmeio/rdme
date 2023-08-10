cf1aef97619e14c32e706e6d59136e9f57c97782
Kanad Gupta <git@kanad.dev>
2023-08-10 12:21:31 -0500

    chore: better link

--- src/lib/configstore.ts
+++ src/lib/configstore.ts
@@ -7,7 +7,7 @@ const configstore = new Configstore(
    * The `VITEST_POOL_ID` and `VITEST_WORKER_ID` are used here to ensure that our `configstore`
    * tests don't clash with one another.
    *
-   * @see {@link https://vitest.dev}
+   * @see {@link https://vitest.dev/guide/migration.html#:~:text=Vitest%20counterparts.-,Envs,-Just%20like%20Jest}
    */
   `${pkg.name}-${process.env.NODE_ENV || 'production'}${process.env.VITEST_POOL_ID || ''}${
     process.env.VITEST_WORKER_ID || ''
