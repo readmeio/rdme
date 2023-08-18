f9e1554f3498ee27104481ad11f700d078d59aa8
Kanad Gupta <git@kanad.dev>
2023-08-18 17:32:42 -0500

    fix: lint

--- __tests__/helpers/jest.matchers.ts
+++ __tests__/helpers/jest.matchers.ts
@@ -14,7 +14,6 @@ interface CustomMatchers<R = unknown> {
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace jest {
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
     interface Matchers<R> extends CustomMatchers<R> {}
   }
 }
