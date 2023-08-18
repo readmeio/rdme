af66cb9e5e58946ba0070a21e9d005cdb99b4f4d
Kanad Gupta <git@kanad.dev>
2023-08-18 17:43:20 -0500

    chore: uninstall knip

--- knip.ts
+++
@@ -1,9 +0,0 @@
-import type { KnipConfig } from 'knip';
-
-const config: KnipConfig = {
-  ignore: ['bin/*.js', 'config/default.js'],
-  ignoreBinaries: ['semantic-release'],
-  ignoreDependencies: ['editor'],
-};
-
-export default config;
