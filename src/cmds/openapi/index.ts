d21785f1c7ed4c64b2770252ab428fc885fcd752
Kanad Gupta <git@kanad.dev>
2023-09-13 16:07:18 -0500

    chore: improved typing on oas prompt

--- src/cmds/openapi/index.ts
+++ src/cmds/openapi/index.ts
@@ -1,4 +1,5 @@
 import type { AuthenticatedCommandOptions } from '../../lib/baseCommand';
+import type { OpenapiPromptOptions } from '../../lib/prompts';
 import type { RequestInit, Response } from 'node-fetch';
 
 import chalk from 'chalk';
@@ -278,10 +279,16 @@ export default class OpenAPICommand extends Command {
       */
 
     function getSpecs(url: string) {
-      return readmeAPIFetch(url, {
-        method: 'get',
-        headers: cleanHeaders(key, selectedVersion),
-      });
+      if (url) {
+        return readmeAPIFetch(url, {
+          method: 'get',
+          headers: cleanHeaders(key, selectedVersion),
+        });
+      }
+
+      throw new Error(
+        'There was an error retrieving your list of API definitions. Please get in touch with us at support@readme.io',
+      );
     }
 
     if (create) {
@@ -312,9 +319,7 @@ export default class OpenAPICommand extends Command {
         return updateSpec(specId);
       }
 
-      // @todo: figure out how to add a stricter type here, see:
-      // https://github.com/readmeio/rdme/pull/570#discussion_r949715913
-      const { option } = await promptTerminal(
+      const { option }: { option: OpenapiPromptOptions } = await promptTerminal(
         promptHandler.createOasPrompt(apiSettingsBody, parsedDocs, totalPages, getSpecs),
       );
       Command.debug(`selection result: ${option}`);
