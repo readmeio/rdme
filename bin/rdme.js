f9b56798ca4e30b18db932051ddab5dce35c49b5
Kanad Gupta <8854718+kanadgupta@users.noreply.github.com>
2023-09-14 18:15:16 -0500

    fix: hide `ExperimentalWarning` (#901)

--- bin/rdme.js
+++ bin/rdme.js
@@ -1,3 +1,5 @@
-#!/usr/bin/env node
+#!/usr/bin/env NODE_OPTIONS=--no-warnings node
+// ^ we need this env variable above to hide the ExperimentalWarnings
+// source: https://github.com/nodejs/node/issues/10802#issuecomment-573376999
 // eslint-disable-next-line import/extensions
 import '../dist/src/cli.js';
