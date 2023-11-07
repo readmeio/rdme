170ce9e1640ce9dd7f69cf28eb0afdfd7b52f23d
Kanad Gupta <git@kanad.dev>
2023-11-07 16:20:40 -0600

    refactor: changelogs

--- __tests__/cmds/changelogs/index.test.ts
+++ __tests__/cmds/changelogs/index.test.ts
@@ -3,13 +3,14 @@ import path from 'node:path';
 
 import chalk from 'chalk';
 import frontMatter from 'gray-matter';
-import nock from 'nock';
+import { http } from 'msw';
+import { setupServer } from 'msw/node';
 import prompts from 'prompts';
-import { describe, beforeAll, afterAll, beforeEach, it, expect, vi } from 'vitest';
+import { describe, beforeAll, afterEach, it, expect, vi, beforeEach } from 'vitest';
 
 import ChangelogsCommand from '../../../src/cmds/changelogs.js';
 import APIError from '../../../src/lib/apiError.js';
-import getAPIMock from '../../helpers/get-api-mock.js';
+import { getAPIPath, validateHeaders } from '../../helpers/get-api-mock.js';
 import hashFileContents from '../../helpers/hash-file-contents.js';
 
 const changelogs = new ChangelogsCommand();
@@ -19,11 +20,83 @@ const fullFixturesDir = `${__dirname}./../../${fixturesBaseDir}`;
 const key = 'API_KEY';
 
 describe('rdme changelogs', () => {
+  let simpleExistingDoc;
+  let anotherExistingDoc;
+  let newDoc;
+
+  const server = setupServer(
+    // existing docs
+    http.get(getAPIPath('/api/v1/changelogs/simple-doc'), ({ request }) => {
+      validateHeaders(request.headers, key);
+      return Response.json({ slug: simpleExistingDoc.slug, lastUpdatedHash: 'anOldHash' });
+    }),
+    http.get(getAPIPath('/api/v1/changelogs/another-doc'), ({ request }) => {
+      validateHeaders(request.headers, key);
+      return Response.json({ slug: anotherExistingDoc.slug, lastUpdatedHash: 'anOldHash' });
+    }),
+    http.put(getAPIPath('/api/v1/changelogs/simple-doc'), ({ request }) => {
+      validateHeaders(request.headers, key);
+      // eslint-disable-next-line vitest/no-standalone-expect
+      expect(request.json()).resolves.toStrictEqual({
+        body: simpleExistingDoc.doc.content,
+        lastUpdatedHash: simpleExistingDoc.hash,
+        ...simpleExistingDoc.doc.data,
+      });
+      return Response.json({ slug: simpleExistingDoc.slug, body: simpleExistingDoc.doc.content });
+    }),
+    http.put(getAPIPath('/api/v1/changelogs/another-doc'), ({ request }) => {
+      validateHeaders(request.headers, key);
+      // eslint-disable-next-line vitest/no-standalone-expect
+      expect(request.json()).resolves.toStrictEqual({
+        body: anotherExistingDoc.doc.content,
+        lastUpdatedHash: anotherExistingDoc.hash,
+        ...anotherExistingDoc.doc.data,
+      });
+      return Response.json({ slug: anotherExistingDoc.slug, body: anotherExistingDoc.doc.content });
+    }),
+    // new doc
+    http.get(getAPIPath('/api/v1/changelogs/new-doc'), ({ request }) => {
+      validateHeaders(request.headers, key);
+      return Response.json(
+        {
+          error: 'CHANGELOG_NOTFOUND',
+          message: "The changelog with the slug 'new-doc' couldn't be found",
+          suggestion: '...a suggestion to resolve the issue...',
+          help: 'If you need help, email support@readme.io and mention log "fake-metrics-uuid".',
+        },
+        { status: 404 },
+      );
+    }),
+    http.post(getAPIPath('/api/v1/changelogs'), async ({ request }) => {
+      validateHeaders(request.headers, key);
+      const body = (await request.json()) as object;
+      return Response.json({
+        _id: '1234',
+        ...body,
+      });
+    }),
+    // new doc with slug specified in front matter
+    http.get(getAPIPath('/api/v1/changelogs/marc-actually-wrote-a-test'), ({ request }) => {
+      validateHeaders(request.headers, key);
+      return Response.json(
+        {
+          error: 'CHANGELOG_NOTFOUND',
+          message: "The changelog with the slug 'new-doc' couldn't be found",
+          suggestion: '...a suggestion to resolve the issue...',
+          help: 'If you need help, email support@readme.io and mention log "fake-metrics-uuid".',
+        },
+        { status: 404 },
+      );
+    }),
+  );
+
   beforeAll(() => {
-    nock.disableNetConnect();
+    server.listen({ onUnhandledRequest: 'error' });
   });
 
-  afterAll(() => nock.cleanAll());
+  afterEach(() => {
+    server.resetHandlers();
+  });
 
   it('should prompt for login if no API key provided', async () => {
     const consoleInfoSpy = vi.spyOn(console, 'info').mockImplementation(() => {});
@@ -63,19 +136,16 @@ describe('rdme changelogs', () => {
   });
 
   describe('existing changelogs', () => {
-    let simpleDoc;
-    let anotherDoc;
-
-    beforeEach(() => {
+    beforeAll(() => {
       let fileContents = fs.readFileSync(path.join(fullFixturesDir, '/existing-docs/simple-doc.md'));
-      simpleDoc = {
+      simpleExistingDoc = {
         slug: 'simple-doc',
         doc: frontMatter(fileContents),
         hash: hashFileContents(fileContents),
       };
 
       fileContents = fs.readFileSync(path.join(fullFixturesDir, '/existing-docs/subdir/another-doc.md'));
-      anotherDoc = {
+      anotherExistingDoc = {
         slug: 'another-doc',
         doc: frontMatter(fileContents),
         hash: hashFileContents(fileContents),
@@ -83,188 +153,102 @@ describe('rdme changelogs', () => {
     });
 
     it('should fetch changelog and merge with what is returned', () => {
-      expect.assertions(1);
+      expect.assertions(3);
 
-      const getMocks = getAPIMock()
-        .get('/api/v1/changelogs/simple-doc')
-        .basicAuth({ user: key })
-        .reply(200, { slug: simpleDoc.slug, lastUpdatedHash: 'anOldHash' })
-        .get('/api/v1/changelogs/another-doc')
-        .basicAuth({ user: key })
-        .reply(200, { slug: anotherDoc.slug, lastUpdatedHash: 'anOldHash' });
-
-      const updateMocks = getAPIMock()
-        .put('/api/v1/changelogs/simple-doc', {
-          body: simpleDoc.doc.content,
-          lastUpdatedHash: simpleDoc.hash,
-          ...simpleDoc.doc.data,
-        })
-        .basicAuth({ user: key })
-        .reply(200, {
-          slug: simpleDoc.slug,
-          body: simpleDoc.doc.content,
-        })
-        .put('/api/v1/changelogs/another-doc', {
-          body: anotherDoc.doc.content,
-          lastUpdatedHash: anotherDoc.hash,
-          ...anotherDoc.doc.data,
-        })
-        .basicAuth({ user: key })
-        .reply(200, { slug: anotherDoc.slug, body: anotherDoc.doc.content });
-
-      return changelogs.run({ filePath: `./__tests__/${fixturesBaseDir}/existing-docs`, key }).then(updatedDocs => {
-        // All changelogs should have been updated because their hashes from the GET request were different from what they
-        // are currently.
-        expect(updatedDocs).toBe(
-          [
-            `✏️ successfully updated 'simple-doc' with contents from __tests__/${fixturesBaseDir}/existing-docs/simple-doc.md`,
-            `✏️ successfully updated 'another-doc' with contents from __tests__/${fixturesBaseDir}/existing-docs/subdir/another-doc.md`,
-          ].join('\n'),
-        );
-
-        getMocks.done();
-        updateMocks.done();
-      });
+      return expect(changelogs.run({ filePath: `./__tests__/${fixturesBaseDir}/existing-docs`, key })).resolves.toBe(
+        [
+          `✏️ successfully updated 'simple-doc' with contents from __tests__/${fixturesBaseDir}/existing-docs/simple-doc.md`,
+          `✏️ successfully updated 'another-doc' with contents from __tests__/${fixturesBaseDir}/existing-docs/subdir/another-doc.md`,
+        ].join('\n'),
+      );
     });
 
     it('should return changelog update info for dry run', () => {
       expect.assertions(1);
 
-      const getMocks = getAPIMock()
-        .get('/api/v1/changelogs/simple-doc')
-        .basicAuth({ user: key })
-        .reply(200, { slug: simpleDoc.slug, lastUpdatedHash: 'anOldHash' })
-        .get('/api/v1/changelogs/another-doc')
-        .basicAuth({ user: key })
-        .reply(200, { slug: anotherDoc.slug, lastUpdatedHash: 'anOldHash' });
-
-      return changelogs
-        .run({ dryRun: true, filePath: `./__tests__/${fixturesBaseDir}/existing-docs`, key })
-        .then(updatedDocs => {
-          // All changelogs should have been updated because their hashes from the GET request were different from what they
-          // are currently.
-          expect(updatedDocs).toBe(
-            [
-              `🎭 dry run! This will update 'simple-doc' with contents from __tests__/${fixturesBaseDir}/existing-docs/simple-doc.md with the following metadata: ${JSON.stringify(
-                simpleDoc.doc.data,
-              )}`,
-              `🎭 dry run! This will update 'another-doc' with contents from __tests__/${fixturesBaseDir}/existing-docs/subdir/another-doc.md with the following metadata: ${JSON.stringify(
-                anotherDoc.doc.data,
-              )}`,
-            ].join('\n'),
-          );
-
-          getMocks.done();
-        });
+      return expect(
+        changelogs.run({ dryRun: true, filePath: `./__tests__/${fixturesBaseDir}/existing-docs`, key }),
+      ).resolves.toBe(
+        [
+          `🎭 dry run! This will update 'simple-doc' with contents from __tests__/${fixturesBaseDir}/existing-docs/simple-doc.md with the following metadata: ${JSON.stringify(
+            simpleExistingDoc.doc.data,
+          )}`,
+          `🎭 dry run! This will update 'another-doc' with contents from __tests__/${fixturesBaseDir}/existing-docs/subdir/another-doc.md with the following metadata: ${JSON.stringify(
+            anotherExistingDoc.doc.data,
+          )}`,
+        ].join('\n'),
+      );
     });
 
-    it('should not send requests for changelogs that have not changed', () => {
-      expect.assertions(1);
+    describe('unchanged changelogs', () => {
+      beforeEach(() => {
+        server.use(
+          http.get(getAPIPath('/api/v1/changelogs/simple-doc'), ({ request }) => {
+            validateHeaders(request.headers, key);
+            return Response.json({ slug: simpleExistingDoc.slug, lastUpdatedHash: simpleExistingDoc.hash });
+          }),
+          http.get(getAPIPath('/api/v1/changelogs/another-doc'), ({ request }) => {
+            validateHeaders(request.headers, key);
+            return Response.json({ slug: anotherExistingDoc.slug, lastUpdatedHash: anotherExistingDoc.hash });
+          }),
+        );
+      });
 
-      const getMocks = getAPIMock()
-        .get('/api/v1/changelogs/simple-doc')
-        .basicAuth({ user: key })
-        .reply(200, { slug: simpleDoc.slug, lastUpdatedHash: simpleDoc.hash })
-        .get('/api/v1/changelogs/another-doc')
-        .basicAuth({ user: key })
-        .reply(200, { slug: anotherDoc.slug, lastUpdatedHash: anotherDoc.hash });
+      it('should not send requests for changelogs that have not changed', () => {
+        expect.assertions(1);
 
-      return changelogs.run({ filePath: `./__tests__/${fixturesBaseDir}/existing-docs`, key }).then(skippedDocs => {
-        expect(skippedDocs).toBe(
+        return expect(changelogs.run({ filePath: `./__tests__/${fixturesBaseDir}/existing-docs`, key })).resolves.toBe(
           [
             '`simple-doc` was not updated because there were no changes.',
             '`another-doc` was not updated because there were no changes.',
           ].join('\n'),
         );
-
-        getMocks.done();
       });
-    });
 
-    it('should adjust "no changes" message if in dry run', () => {
-      expect.assertions(1);
+      it('should adjust "no changes" message if in dry run', () => {
+        expect.assertions(1);
 
-      const getMocks = getAPIMock()
-        .get('/api/v1/changelogs/simple-doc')
-        .basicAuth({ user: key })
-        .reply(200, { slug: simpleDoc.slug, lastUpdatedHash: simpleDoc.hash })
-        .get('/api/v1/changelogs/another-doc')
-        .basicAuth({ user: key })
-        .reply(200, { slug: anotherDoc.slug, lastUpdatedHash: anotherDoc.hash });
-
-      return changelogs
-        .run({ dryRun: true, filePath: `./__tests__/${fixturesBaseDir}/existing-docs`, key })
-        .then(skippedDocs => {
-          expect(skippedDocs).toBe(
-            [
-              '🎭 dry run! `simple-doc` will not be updated because there were no changes.',
-              '🎭 dry run! `another-doc` will not be updated because there were no changes.',
-            ].join('\n'),
-          );
-
-          getMocks.done();
-        });
+        return expect(
+          changelogs.run({ dryRun: true, filePath: `./__tests__/${fixturesBaseDir}/existing-docs`, key }),
+        ).resolves.toBe(
+          [
+            '🎭 dry run! `simple-doc` will not be updated because there were no changes.',
+            '🎭 dry run! `another-doc` will not be updated because there were no changes.',
+          ].join('\n'),
+        );
+      });
     });
   });
 
   describe('new changelogs', () => {
-    it('should create new changelog', async () => {
-      const slug = 'new-doc';
-      const id = '1234';
-      const doc = frontMatter(fs.readFileSync(path.join(fullFixturesDir, `/new-docs/${slug}.md`)));
-      const hash = hashFileContents(fs.readFileSync(path.join(fullFixturesDir, `/new-docs/${slug}.md`)));
-
-      const getMock = getAPIMock()
-        .get(`/api/v1/changelogs/${slug}`)
-        .basicAuth({ user: key })
-        .reply(404, {
-          error: 'CHANGELOG_NOTFOUND',
-          message: `The changelog with the slug '${slug}' couldn't be found`,
-          suggestion: '...a suggestion to resolve the issue...',
-          help: 'If you need help, email support@readme.io and mention log "fake-metrics-uuid".',
-        });
-
-      const postMock = getAPIMock()
-        .post('/api/v1/changelogs', { slug, body: doc.content, ...doc.data, lastUpdatedHash: hash })
-        .basicAuth({ user: key })
-        .reply(201, { slug, _id: id, body: doc.content, ...doc.data, lastUpdatedHash: hash });
-
-      await expect(changelogs.run({ filePath: `./__tests__/${fixturesBaseDir}/new-docs`, key })).resolves.toBe(
-        `🌱 successfully created 'new-doc' (ID: 1234) with contents from __tests__/${fixturesBaseDir}/new-docs/new-doc.md`,
-      );
-
-      getMock.done();
-      postMock.done();
+    beforeAll(() => {
+      const fileContents = fs.readFileSync(path.join(fullFixturesDir, '/new-docs/new-doc.md'));
+      newDoc = {
+        slug: 'new-doc',
+        doc: frontMatter(fileContents),
+        hash: hashFileContents(fileContents),
+      };
     });
 
-    it('should return creation info for dry run', async () => {
-      const slug = 'new-doc';
-      const doc = frontMatter(fs.readFileSync(path.join(fullFixturesDir, `/new-docs/${slug}.md`)));
+    it('should create new changelog', () => {
+      return expect(changelogs.run({ filePath: `./__tests__/${fixturesBaseDir}/new-docs`, key })).resolves.toBe(
+        `🌱 successfully created 'new-doc' (ID: 1234) with contents from __tests__/${fixturesBaseDir}/new-docs/new-doc.md`,
+      );
+    });
 
-      const getMock = getAPIMock()
-        .get(`/api/v1/changelogs/${slug}`)
-        .basicAuth({ user: key })
-        .reply(404, {
-          error: 'CHANGELOG_NOTFOUND',
-          message: `The changelog with the slug '${slug}' couldn't be found`,
-          suggestion: '...a suggestion to resolve the issue...',
-          help: 'If you need help, email support@readme.io and mention log "fake-metrics-uuid".',
-        });
-
-      await expect(
+    it('should return creation info for dry run', () => {
+      return expect(
         changelogs.run({ dryRun: true, filePath: `./__tests__/${fixturesBaseDir}/new-docs`, key }),
       ).resolves.toBe(
         `🎭 dry run! This will create 'new-doc' with contents from __tests__/${fixturesBaseDir}/new-docs/new-doc.md with the following metadata: ${JSON.stringify(
-          doc.data,
+          newDoc.doc.data,
         )}`,
       );
-
-      getMock.done();
     });
 
-    it('should fail if any changelogs are invalid', async () => {
-      const folder = 'failure-docs';
+    it('should fail if any changelogs are invalid', () => {
       const slug = 'new-doc';
+      const fullDirectory = `__tests__/${fixturesBaseDir}/failure-docs`;
 
       const errorObject = {
         error: 'CHANGELOG_INVALID',
@@ -274,69 +258,27 @@ describe('rdme changelogs', () => {
         help: "If you need help, email support@readme.io and include the following link to your API log: 'fake-metrics-uuid'.",
       };
 
-      const doc = frontMatter(fs.readFileSync(path.join(fullFixturesDir, `/${folder}/${slug}.md`)));
-
-      const hash = hashFileContents(fs.readFileSync(path.join(fullFixturesDir, `/${folder}/${slug}.md`)));
-
-      const getMocks = getAPIMock()
-        .get(`/api/v1/changelogs/${slug}`)
-        .basicAuth({ user: key })
-        .reply(404, {
-          error: 'CHANGELOG_NOTFOUND',
-          message: `The changelog with the slug '${slug}' couldn't be found`,
-          suggestion: '...a suggestion to resolve the issue...',
-          help: 'If you need help, email support@readme.io and mention log "fake-metrics-uuid".',
-        });
-
-      const postMocks = getAPIMock()
-        .post('/api/v1/changelogs', { slug, body: doc.content, ...doc.data, lastUpdatedHash: hash })
-        .basicAuth({ user: key })
-        .reply(400, errorObject);
-
-      const fullDirectory = `__tests__/${fixturesBaseDir}/${folder}`;
+      server.use(
+        http.post(getAPIPath('/api/v1/changelogs'), ({ request }) => {
+          validateHeaders(request.headers, key);
+          return Response.json(errorObject, { status: 400 });
+        }),
+      );
 
       const formattedErrorObject = {
         ...errorObject,
         message: `Error uploading ${chalk.underline(`${fullDirectory}/${slug}.md`)}:\n\n${errorObject.message}`,
       };
 
-      await expect(changelogs.run({ filePath: `./${fullDirectory}`, key })).rejects.toStrictEqual(
+      return expect(changelogs.run({ filePath: `./${fullDirectory}`, key })).rejects.toStrictEqual(
         new APIError(formattedErrorObject),
       );
-
-      getMocks.done();
-      postMocks.done();
     });
-  });
 
-  describe('slug metadata', () => {
-    it('should use provided slug', async () => {
-      const slug = 'new-doc-slug';
-      const id = '1234';
-      const doc = frontMatter(fs.readFileSync(path.join(fullFixturesDir, `/slug-docs/${slug}.md`)));
-      const hash = hashFileContents(fs.readFileSync(path.join(fullFixturesDir, `/slug-docs/${slug}.md`)));
-
-      const getMock = getAPIMock()
-        .get(`/api/v1/changelogs/${doc.data.slug}`)
-        .basicAuth({ user: key })
-        .reply(404, {
-          error: 'CHANGELOG_NOTFOUND',
-          message: `The changelog with the slug '${slug}' couldn't be found`,
-          suggestion: '...a suggestion to resolve the issue...',
-          help: 'If you need help, email support@readme.io and mention log "fake-metrics-uuid".',
-        });
-
-      const postMock = getAPIMock()
-        .post('/api/v1/changelogs', { slug, body: doc.content, ...doc.data, lastUpdatedHash: hash })
-        .basicAuth({ user: key })
-        .reply(201, { slug: doc.data.slug, _id: id, body: doc.content, ...doc.data, lastUpdatedHash: hash });
-
-      await expect(changelogs.run({ filePath: `./__tests__/${fixturesBaseDir}/slug-docs`, key })).resolves.toBe(
+    it('should use slug provided in frontmatter', () => {
+      return expect(changelogs.run({ filePath: `./__tests__/${fixturesBaseDir}/slug-docs`, key })).resolves.toBe(
         `🌱 successfully created 'marc-actually-wrote-a-test' (ID: 1234) with contents from __tests__/${fixturesBaseDir}/slug-docs/new-doc-slug.md`,
       );
-
-      getMock.done();
-      postMock.done();
     });
   });
 });
