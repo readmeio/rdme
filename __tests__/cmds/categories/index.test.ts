00d1d31403c9f5c77be0414a8befbb2aa4e6e4b1
Kanad Gupta <git@kanad.dev>
2023-11-07 10:14:49 -0600

    refactor: categories

--- __tests__/cmds/categories/index.test.ts
+++ __tests__/cmds/categories/index.test.ts
@@ -1,9 +1,10 @@
-import nock from 'nock';
+import { http } from 'msw';
+import { setupServer } from 'msw/node';
 import prompts from 'prompts';
 import { describe, beforeAll, afterEach, it, expect, vi } from 'vitest';
 
 import CategoriesCommand from '../../../src/cmds/categories/index.js';
-import getAPIMock, { getAPIMockWithVersionHeader } from '../../helpers/get-api-mock.js';
+import { getAPIPath, validateHeaders } from '../../helpers/get-api-mock.js';
 
 const categories = new CategoriesCommand();
 
@@ -11,11 +12,20 @@ const key = 'API_KEY';
 const version = '1.0.0';
 
 describe('rdme categories', () => {
+  const server = setupServer(
+    http.get(getAPIPath(`/api/v1/version/${version}`), ({ request }) => {
+      validateHeaders(request.headers, key);
+      return Response.json({ version });
+    }),
+  );
+
   beforeAll(() => {
-    nock.disableNetConnect();
+    server.listen({ onUnhandledRequest: 'error' });
   });
 
-  afterEach(() => nock.cleanAll());
+  afterEach(() => {
+    server.resetHandlers();
+  });
 
   it('should prompt for login if no API key provided', async () => {
     const consoleInfoSpy = vi.spyOn(console, 'info').mockImplementation(() => {});
@@ -34,42 +44,50 @@ describe('rdme categories', () => {
     delete process.env.TEST_RDME_CI;
   });
 
-  it('should return all categories for a single page', async () => {
-    const getMock = getAPIMockWithVersionHeader(version)
-      .persist()
-      .get('/api/v1/categories?perPage=20&page=1')
-      .basicAuth({ user: key })
-      .reply(200, [{ title: 'One Category', slug: 'one-category', type: 'guide' }], {
-        'x-total-count': '1',
-      });
-
-    const versionMock = getAPIMock().get(`/api/v1/version/${version}`).basicAuth({ user: key }).reply(200, { version });
-
-    await expect(categories.run({ key, version: '1.0.0' })).resolves.toBe(
-      JSON.stringify([{ title: 'One Category', slug: 'one-category', type: 'guide' }], null, 2),
+  it('should return all categories for a single page', () => {
+    server.use(
+      http.get(getAPIPath('/api/v1/categories'), ({ request }) => {
+        validateHeaders(request.headers, key, { 'x-readme-version': version });
+        return Response.json([{ title: 'One Category', slug: 'one-category', type: 'guide' }], {
+          headers: { 'x-total-count': '1' },
+        });
+      }),
     );
 
-    getMock.done();
-    versionMock.done();
+    return expect(categories.run({ key, version: '1.0.0' })).resolves.toBe(
+      JSON.stringify([{ title: 'One Category', slug: 'one-category', type: 'guide' }], null, 2),
+    );
   });
 
-  it('should return all categories for multiple pages', async () => {
-    const getMock = getAPIMockWithVersionHeader(version)
-      .persist()
-      .get('/api/v1/categories?perPage=20&page=1')
-      .basicAuth({ user: key })
-      .reply(200, [{ title: 'One Category', slug: 'one-category', type: 'guide' }], {
-        'x-total-count': '21',
-      })
-      .get('/api/v1/categories?perPage=20&page=2')
-      .basicAuth({ user: key })
-      .reply(200, [{ title: 'Another Category', slug: 'another-category', type: 'guide' }], {
-        'x-total-count': '21',
-      });
+  it('should return all categories for multiple pages', () => {
+    server.use(
+      http.get(getAPIPath('/api/v1/categories'), ({ request }) => {
+        validateHeaders(request.headers, key, { 'x-readme-version': version });
+        const { searchParams } = new URL(request.url);
+        if (searchParams.get('perPage') !== '20') {
+          return new Response('unexpected perPage query parameter', { status: 400 });
+        }
+        let payload = [];
+        switch (searchParams.get('page')) {
+          case '1':
+            payload = [{ title: 'One Category', slug: 'one-category', type: 'guide' }];
+            break;
 
-    const versionMock = getAPIMock().get(`/api/v1/version/${version}`).basicAuth({ user: key }).reply(200, { version });
+          case '2':
+            payload = [{ title: 'Another Category', slug: 'another-category', type: 'guide' }];
+            break;
 
-    await expect(categories.run({ key, version: '1.0.0' })).resolves.toBe(
+          default:
+            return new Response('unexpected page query parameter', { status: 400 });
+        }
+
+        return Response.json(payload, {
+          headers: { 'x-total-count': '21' },
+        });
+      }),
+    );
+
+    return expect(categories.run({ key, version: '1.0.0' })).resolves.toBe(
       JSON.stringify(
         [
           { title: 'One Category', slug: 'one-category', type: 'guide' },
@@ -79,8 +97,5 @@ describe('rdme categories', () => {
         2,
       ),
     );
-
-    getMock.done();
-    versionMock.done();
   });
 });
