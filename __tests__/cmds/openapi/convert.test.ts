b1a178e688b92dd948e9c2dc6407149728e6f073
Kanad Gupta <git@kanad.dev>
2023-11-21 15:57:27 -0600

    fix(openapi/convert): support openapi in convert command

--- __tests__/cmds/openapi/convert.test.ts
+++ __tests__/cmds/openapi/convert.test.ts
@@ -7,7 +7,7 @@ import OpenAPIConvertCommand from '../../../src/cmds/openapi/convert.js';
 
 const convert = new OpenAPIConvertCommand();
 
-const successfulConversion = () => 'Your converted API definition has been saved to output.json!';
+const successfulConversion = () => 'Your API definition has been converted and bundled and saved to output.json!';
 
 describe('rdme openapi:convert', () => {
   describe('converting', () => {
@@ -41,13 +41,45 @@ describe('rdme openapi:convert', () => {
     it.each([['json'], ['yaml']])('should fail if given an OpenAPI 3.0 definition (format: %s)', async format => {
       const spec = require.resolve(`@readme/oas-examples/3.0/${format}/petstore.${format}`);
 
+      const consoleWarnSpy = vi.spyOn(console, 'warn').mockImplementation(() => {});
+
+      let reducedSpec;
+      fs.writeFileSync = vi.fn((fileName, data) => {
+        reducedSpec = JSON.parse(data as string);
+      });
+
+      prompts.inject(['output.json']);
+
       await expect(
         convert.run({
           spec,
         }),
-      ).rejects.toStrictEqual(
-        new Error("Sorry, this API definition is already an OpenAPI definition and doesn't need to be converted."),
+      ).resolves.toBe(successfulConversion());
+
+      expect(fs.writeFileSync).toHaveBeenCalledWith('output.json', expect.any(String));
+      expect(reducedSpec.tags).toHaveLength(3);
+      expect(Object.keys(reducedSpec.paths)).toStrictEqual([
+        '/pet',
+        '/pet/findByStatus',
+        '/pet/findByTags',
+        '/pet/{petId}',
+        '/pet/{petId}/uploadImage',
+        '/store/inventory',
+        '/store/order',
+        '/store/order/{orderId}',
+        '/user',
+        '/user/createWithArray',
+        '/user/createWithList',
+        '/user/login',
+        '/user/logout',
+        '/user/{username}',
+      ]);
+      expect(Object.keys(reducedSpec.paths['/pet/{petId}'])).toStrictEqual(['get', 'post', 'delete']);
+      expect(consoleWarnSpy).toHaveBeenCalledWith(
+        '⚠️  Warning! The input file is already OpenAPI, so no conversion is necessary. Any external references will be bundled.',
       );
+
+      consoleWarnSpy.mockRestore();
     });
   });
 });
