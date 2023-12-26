1dfac0a2f345c1b9d906ff322fb71248c0292e15
Kanad Gupta <git@kanad.dev>
2023-12-25 20:27:28 -0600

    fix: return command output in wrapper
    this is probably where should do some error handling too but not sure how yet

--- bin/dev.js
+++ bin/dev.js
@@ -2,7 +2,13 @@
 
 async function main() {
   const { execute } = await import('@oclif/core');
-  await execute({ development: true, dir: import.meta.url });
+  await execute({ development: true, dir: import.meta.url }).then(msg => {
+    if (msg) {
+      // eslint-disable-next-line no-console
+      console.log(msg);
+    }
+    return process.exit(0);
+  });
 }
 
 await main();
