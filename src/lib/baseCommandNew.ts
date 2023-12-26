161a81067da60fc855ee5d8089af11bd27728654
Kanad Gupta <git@kanad.dev>
2023-12-25 18:33:58 -0600

    revert: don't add error handling here yet
    this was causing errors in tests, will have to tackle this a different way

--- src/lib/baseCommandNew.ts
+++ src/lib/baseCommandNew.ts
@@ -1,7 +1,6 @@
 import type { Interfaces } from '@oclif/core';
 
 import { Command as OclifCommand } from '@oclif/core';
-import chalk from 'chalk';
 
 export enum CommandCategories {
   ADMIN = 'admin',
@@ -40,41 +39,10 @@ export default abstract class BaseCommand<T extends typeof OclifCommand> extends
 
   protected flags!: Flags<T>;
 
-  // eslint-disable-next-line class-methods-use-this
   protected async catch(err: Error & { exitCode?: number }): Promise<unknown> {
     // add any custom logic to handle errors from the command
     // or simply return the parent class error handling
-    // return super.catch(err);
-
-    let message = `Yikes, something went wrong! Please try again and if the problem persists, get in touch with our support team at ${chalk.underline(
-      'support@readme.io',
-    )}.`;
-
-    if (err.message) {
-      message = err.message;
-    }
-
-    /**
-     * If we're in a GitHub Actions environment, log errors with that formatting instead.
-     *
-     * @see {@link https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-error-message}
-     * @see {@link https://github.com/actions/toolkit/tree/main/packages/core#annotations}
-     */
-    // if (isGHA()) {
-    //   return core.setFailed(message);
-    // }
-
-    // If this is a soft error then we should output the result as a regular log but exit the CLI
-    // with an error status code.
-    if (err.name === 'SoftError') {
-      // eslint-disable-next-line no-console
-      console.log(err.message);
-    } else {
-      // eslint-disable-next-line no-console
-      console.error(chalk.red(`\n${message}\n`));
-    }
-
-    return process.exit(1);
+    return super.catch(err);
   }
 
   // copied from here: https://oclif.io/docs/base_class
