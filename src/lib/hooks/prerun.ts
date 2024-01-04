3ee9794ba6c288638e6b700d9c1d20026627ef57
Kanad Gupta <git@kanad.dev>
2024-01-03 19:46:03 -0600

    feat: better debugging in prerun hook and configstore retrieval

--- src/lib/hooks/prerun.ts
+++ src/lib/hooks/prerun.ts
@@ -9,11 +9,14 @@ import { info } from '../logger.js';
 import loginFlow from '../loginFlow.js';
 
 const hook: Hook<'prerun'> = async function run(options) {
+  this.debug('configstore location:', configstore.path);
   if (options.Command?.flags?.key) {
+    this.debug('current command has --key flag');
     // eslint-disable-next-line no-param-reassign
     options.Command.flags.key = Flags.string({
-      async parse(input) {
-        const { email, project } = getCurrentConfig();
+      parse: async input => {
+        this.debug('--key flag detected in parse function');
+        const { email, project } = getCurrentConfig.call(this);
         // We only want to log this if the API key is stored in the configstore, **not** in an env var.
         if (input && configstore.get('apiKey') === input) {
           info(
@@ -27,9 +30,13 @@ const hook: Hook<'prerun'> = async function run(options) {
       },
       // `default` is run if no `--key` flag is passed
       default: async () => {
-        const { apiKey } = getCurrentConfig();
+        this.debug('no --key flag detected, running default function');
+        const { apiKey } = getCurrentConfig.call(this);
         // if the user is passing an API key via env var or configstore, use that
-        if (apiKey) return apiKey;
+        if (apiKey) {
+          this.debug('api key found in config, returning');
+          return apiKey;
+        }
         if (isCI()) {
           // TODO make sure this error throws properly
           // this.error('No project API key provided. Please use `--key`.');
@@ -43,13 +50,17 @@ const hook: Hook<'prerun'> = async function run(options) {
         return configstore.get('apiKey');
       },
     });
+  } else {
+    this.debug('current command does not have --key flag');
   }
 
   // the logic in this block is a little weird it does two things:
   // 1. throws if the user is attempting to use --github in a CI environment
   // 2. resets the --github flag options to the default in certain tests
   if (options.Command?.flags?.github) {
+    this.debug('current command has --github flag');
     if (isCI()) {
+      this.debug('in CI envirnoment');
       // eslint-disable-next-line no-param-reassign
       options.Command.flags.github = Flags.boolean({
         async parse() {
@@ -61,6 +72,8 @@ const hook: Hook<'prerun'> = async function run(options) {
       // eslint-disable-next-line no-param-reassign
       options.Command.flags.github = githubFlag;
     }
+  } else {
+    this.debug('current command does not have --github flag');
   }
 };
 
