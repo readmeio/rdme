a8b01e25ee20d45e34c3c187a9905c4ce2a5bf19
Kanad Gupta <git@kanad.dev>
2024-01-04 15:58:22 -0600

    fix: output result in top-level runner

--- bin/run.js
+++ bin/run.js
@@ -2,7 +2,12 @@
 
 async function main() {
   const { execute } = await import('@oclif/core');
-  await execute({ dir: import.meta.url });
+  await execute({ dir: import.meta.url }).then(msg => {
+    if (msg) {
+      // eslint-disable-next-line no-console
+      console.log(msg);
+    }
+  });
 }
 
 await main();
