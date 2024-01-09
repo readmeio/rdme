e92a2b3e07a049bf82dbfd1126488c1e761e3b1c
Kanad Gupta <git@kanad.dev>
2024-01-09 14:50:24 -0600

    chore: swap out when we surface node warnings

--- bin/dev.js
+++ bin/dev.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S node --loader ts-node/esm --no-warnings=ExperimentalWarning
+#!/usr/bin/env -S node --loader ts-node/esm
 
 async function main() {
   const { execute } = await import('@oclif/core');
--- bin/run.js
+++ bin/run.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env node  --no-warnings=ExperimentalWarning
 
 async function main() {
   const { execute } = await import('@oclif/core');
