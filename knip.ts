58bcc4806197ee176dcf3e232d9ad7a9a31e4d33
Kanad Gupta <8854718+kanadgupta@users.noreply.github.com>
2024-02-01 12:28:29 -0600

    chore(deps): `npm up` (#971)
    * chore(deps): `npm up`
    
    * chore: bump knip to the latest
    
    * chore(deps): husky@9
    
    migration steps grabbed from here: https://github.com/typicode/husky/releases/tag/v9.0.1
    
    * ci: lint

--- knip.ts
+++ knip.ts
@@ -1,8 +1,10 @@
 import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
-  ignore: ['bin/*.js', 'vitest.single-threaded.config.ts'],
+  ignore: ['bin/*.js'],
   ignoreBinaries: ['semantic-release'],
+  // used in husky commit hooks
+  ignoreDependencies: ['@commitlint/cli'],
 };
 
 export default config;
