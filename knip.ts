2a39a906e87b9063e2f29a8f305d548b5c66263e
Kanad Gupta <git@kanad.dev>
2024-03-01 13:05:08 -0600

    chore(deps): knip

--- knip.ts
+++ knip.ts
@@ -3,8 +3,6 @@ import type { KnipConfig } from 'knip';
 const config: KnipConfig = {
   ignore: ['bin/*.js'],
   ignoreBinaries: ['semantic-release'],
-  // used in husky commit hooks
-  ignoreDependencies: ['@commitlint/cli'],
 };
 
 export default config;
