1713d6b3a9becf53b01e8ffcd1a73b4949d20a91
Kanad Gupta <8854718+kanadgupta@users.noreply.github.com>
2024-05-01 11:03:57 -0500

    ci: vitest workspaces (#998)
    ## 🧰 Changes
    
    > [!IMPORTANT]
    > **this continues to be a vitest supremacy account 😤**
    
    In this repo, we have tests that use `process.chdir()`, so we maintain a
    separate Vitest config for running those via a
    [`forks`](https://vitest.dev/config/#forks) pool vs. the standard
    [`threads`](https://vitest.dev/config/#threads) pool — see
    https://github.com/readmeio/rdme/pull/870 for more background on this.
    
    We've used two separate npm scripts (i.e., `npm run test:multi && npm
    run test:single`) to run our tests sequentially. I discovered this
    morning that [Vitest's workspaces
    feature](https://vitest.dev/guide/workspace) isn't just for monorepos —
    it's actually the recommended way to maintain multiple test
    configurations no matter the context.
    
    In this PR, I consolidated our two test configs into a single workspace
    config. We get a few nice benefits by leveraging workspaces:
    - No longer needing to manually pass in the correct config file when
    running certain test files (e.g., `npx vitest --config
    vitest.single-threaded.config.ts [file]`). In other words, `npx vitest
    [file]` ✨ just works ✨, which should make it a lot easier for new
    contributors to run tests
    - Much cleaner test output — we now can see coverage across all files,
    rather than two separate coverage reports[^1]
    - Easier to share options between the two configs
    - A single config file to rule them all
    - Tests seem to be faster (but hard to say)
    
    ## 🧬 QA & Testing
    
    Do all the tests run? And do they still run and pass properly? Here's
    the latest test run in `next` for comparison:
    https://github.com/readmeio/rdme/actions/runs/8910638856
    
    [^1]: Small downside to this — [workspaces don't allow you to configure
    coverage options](https://vitest.dev/guide/workspace#coverage). Not a
    huge deal since our coverage is great in this repo and we can stick to
    the defaults and it still addresses our needs.

---
+++ vitest.workspace.ts
@@ -0,0 +1,48 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineWorkspace } from 'vitest/config';
+
+const sharedExclude = [
+  '**/dist/**',
+  '**/node_modules/**',
+  '**/__fixtures__/**',
+  '**/helpers/**',
+  '**/__snapshots__/**',
+];
+
+const sharedOpts = {
+  globalSetup: ['./__tests__/setup.js'],
+  setupFiles: ['./__tests__/helpers/vitest.matchers.ts'],
+};
+
+export default defineWorkspace([
+  {
+    /**
+     * Our default test suite
+     */
+    test: {
+      exclude: [...sharedExclude, '**/single-threaded/**'],
+      name: 'default',
+      ...sharedOpts,
+    },
+  },
+  {
+    /**
+     * Our single-threaded test suite.
+     * Only tests that use `process.chdir()` should be run using this config.
+     */
+    test: {
+      exclude: sharedExclude,
+      include: ['**/single-threaded/**'],
+      name: 'single-threaded',
+      /**
+       * We can't run tests with `threads` on because we use `process.chdir()` in some tests and
+       * that isn't available in worker threads, and it's way too much work to mock out an entire
+       * filesystem and `fs` calls for the tests that use it.
+       *
+       * @see {@link https://github.com/vitest-dev/vitest/issues/566}
+       */
+      pool: 'forks',
+      ...sharedOpts,
+    },
+  },
+]);
