a3f52d55f04debcf11d120a97311e8ecfcfa4c88
Kanad Gupta <git@kanad.dev>
2024-05-01 07:57:39 -0500

    Revert "chore(deps): bump knip"
    This reverts commit e0ad60ee8e16074c452bac35c89823e2e706a266.

--- knip.ts
+++ knip.ts
@@ -3,6 +3,7 @@ import type { KnipConfig } from 'knip';
 const config: KnipConfig = {
   ignore: ['bin/*.js'],
   ignoreBinaries: ['semantic-release'],
+  ignoreDependencies: ['@vitest/coverage-v8'],
 };
 
 export default config;
