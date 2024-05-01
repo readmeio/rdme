326ba086e4689e9587a68616ec2023e28f91ef2e
Kanad Gupta <git@kanad.dev>
2024-05-01 07:57:46 -0500

    Revert "fix: knip"
    This reverts commit a24781ee127ed90430bab40549b41e9453ec69fb.

--- knip.ts
+++ knip.ts
@@ -3,7 +3,6 @@ import type { KnipConfig } from 'knip';
 const config: KnipConfig = {
   ignore: ['bin/*.js'],
   ignoreBinaries: ['semantic-release'],
-  ignoreDependencies: ['@vitest/coverage-v8'],
 };
 
 export default config;
