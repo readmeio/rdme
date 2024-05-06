1be03902c70850be5b09d7128598118ab32d6ee4
Kanad Gupta <git@kanad.dev>
2024-05-06 09:55:42 -0500

    chore: whoops also remove this

--- __tests__/helpers/get-api-mock.ts
+++ __tests__/helpers/get-api-mock.ts
@@ -1,16 +1,7 @@
-import type { Headers } from 'node-fetch';
-
 import nock from 'nock';
 
 import config from '../../src/lib/config.js';
 import { getUserAgent } from '../../src/lib/readmeAPIFetch.js';
-
-/**
- * A type describing a raw object of request headers.
- * We use this in our API request mocking to validate that the request
- * contains all the expected headers.
- */
-type ReqHeaders = Record<string, unknown>;
 
 /**
  * Nock wrapper that adds required `user-agent` request header
@@ -30,29 +21,4 @@ export function getAPIMockWithVersionHeader(v: string) {
   return getAPIMock({
     'x-readme-version': v,
   });
-}
-
-function validateHeaders(headers: Headers, basicAuthUser: string, expectedReqHeaders: ReqHeaders) {
-  // validate all headers in expectedReqHeaders
-  Object.keys(expectedReqHeaders).forEach(reqHeaderKey => {
-    if (headers.get(reqHeaderKey) !== expectedReqHeaders[reqHeaderKey]) {
-      throw new Error(
-        `Expected the request header '${expectedReqHeaders[reqHeaderKey]}', received '${headers.get(reqHeaderKey)}'`,
-      );
-    }
-  });
-
-  // validate basic auth header
-  if (basicAuthUser) {
-    const encodedApiKey = headers.get('Authorization').split(' ')[1];
-    const decodedApiKey = Buffer.from(encodedApiKey, 'base64').toString();
-    if (decodedApiKey !== `${basicAuthUser}:`) {
-      throw new Error(`Expected API key '${basicAuthUser}', received '${decodedApiKey}'`);
-    }
-  }
-
-  const userAgent = headers.get('user-agent');
-  if (userAgent !== getUserAgent()) {
-    throw new Error(`Expected user agent '${getUserAgent()}', received '${userAgent}'`);
-  }
 }
