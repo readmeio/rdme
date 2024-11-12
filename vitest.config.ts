93a989b985a494b7df73ef8b49bf67899cff0bc2
Kanad Gupta <8854718+kanadgupta@users.noreply.github.com>
2024-11-12 15:27:52 -0600

    test: remove single-threaded tests (#1065)
    | 🚥 Resolves ISSUE_ID |
    | :------------------- |
    
    ## 🧰 Changes
    
    before `vitest@2` made the default pool type `forks`, we split out our
    tests for the sake of "making our testbed faster". it was cumbersome to
    maintain and in reality the testbed gains were minimal at best. instead,
    i'm just consolidating everything into one test suite again.
    
    ## 🧬 QA & Testing
    
    do tests pass?

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    exclude: ['**/dist/**', '**/node_modules/**', '**/__fixtures__/**', '**/helpers/**', '**/__snapshots__/**'],
+    globalSetup: ['./__tests__/setup.js'],
+    setupFiles: ['./__tests__/helpers/vitest.matchers.ts'],
+  },
+});
