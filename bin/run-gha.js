97e01217d6c6431243476c779ae400e6b34b9534
Kanad Gupta <git@kanad.dev>
2024-11-15 23:50:05 -0600

    chore: bring in dep changes, rollup config

---
+++ bin/run-gha.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node --no-warnings=ExperimentalWarning
+// ^ we need this env variable above to hide the ExperimentalWarnings
+// source: https://github.com/nodejs/node/issues/10802#issuecomment-573376999
+
+async function main() {
+  const { execute } = await import('@oclif/core');
+  await execute({ dir: import.meta.url, args: process.env.INPUT_RDME?.split(' ') || [] }).then(msg => {
+    if (msg) {
+      // eslint-disable-next-line no-console
+      console.log(msg);
+    }
+  });
+}
+
+main();
