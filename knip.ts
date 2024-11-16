1233e1b3e356a6687a4d832078c04dde48f93db7
Kanad Gupta <git@kanad.dev>
2024-11-16 00:07:47 -0600

    fix: knip

--- knip.ts
+++ knip.ts
@@ -2,7 +2,7 @@ import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
   entry: ['src/index.ts', 'src/lib/hooks/*.ts', 'src/lib/help.ts'],
-  ignore: ['bin/*.js'],
+  ignore: ['bin/*.js', 'gha-dist/**'],
   ignoreBinaries: ['semantic-release'],
   ignoreDependencies: ['oclif'],
 };
