6677c5f2612ef58a9b4e3e519c75e66357bdd932
Kanad Gupta <git@kanad.dev>
2024-11-16 09:00:29 -0600

    chore: simplify knip

--- knip.ts
+++ knip.ts
@@ -1,7 +1,7 @@
 import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
-  entry: ['src/index.ts', 'src/lib/hooks/*.ts', 'src/lib/help.ts'],
+  entry: ['src/index.ts', 'src/lib/help.ts'],
   ignore: ['bin/*.js'],
   ignoreBinaries: ['semantic-release'],
   ignoreDependencies: ['oclif'],
