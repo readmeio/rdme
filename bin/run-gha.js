ec30bac0ac0f92a84a812e65d790a7bb2879e42c
Kanad Gupta <git@kanad.dev>
2024-11-16 00:13:22 -0600

    fix: use string-argv to parse args

--- bin/run-gha.js
+++ bin/run-gha.js
@@ -2,9 +2,12 @@
 // ^ we need this env variable above to hide the ExperimentalWarnings
 // source: https://github.com/nodejs/node/issues/10802#issuecomment-573376999
 
+import stringArgv from 'string-argv';
+
 async function main() {
   const { execute } = await import('@oclif/core');
-  await execute({ dir: import.meta.url, args: process.env.INPUT_RDME?.split(' ') || [] }).then(msg => {
+  const args = process.env.INPUT_RDME ? stringArgv(process.env.INPUT_RDME) : [];
+  await execute({ args, dir: import.meta.url }).then(msg => {
     if (msg) {
       // eslint-disable-next-line no-console
       console.log(msg);
