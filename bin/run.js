b155c0ec4dff927eba5208746d624534ad37ffd7
Kanad Gupta <8854718+kanadgupta@users.noreply.github.com>
2024-11-18 10:04:28 -0600

    ci: oclif mess around (#1067)
    | 🚥 Resolves ISSUE_ID |
    | :------------------- |
    
    ## 🧰 Changes
    
    (still in-progress) separate PR from
    [#1068](https://github.com/readmeio/rdme/pull/1068) with a new
    architecture for github actions that plays nicely with oclif.
    
    outstanding work:
    - [ ] remove all docker build/tagging/release code
    - [ ] update `semantic-release` workflow to support this new process.
    here are the assets we'll need to consider every time we tag a release
    ([link](https://github.com/readmeio/rdme/blob/d01d76fe3c2e4a98b4f5c415be03e589fbe3b56e/.releaserc.yml#L30)):
      - [ ] command list README (i.e., the one generated by `oclif readme`
    - [ ] we do **not** need to update `action.yml` on every release anymore
    - [ ] everything in `dist-gha` (the two JS files ~~and `package.json`~~)
    - [ ] (do we need an `oclif` manifest in this directory? probably not?)
    - [ ] we might not actually need to track this duplicated `package.json`
    — what if we used the
    [`runs.pre`](https://docs.github.com/en/actions/sharing-automations/creating-actions/metadata-syntax-for-github-actions#runspre)
    script to generate it?
      - [ ] package/package-lock.json
      - [ ] CHANGELOG.md
    - [ ] `oclif` manifest (this one is interesting... we probably don't
    need to git track it but it should be included in our npm assets)
    
    ## 🧬 QA & Testing
    
    Provide as much information as you can on how to test what you've done.

--- bin/run.js
+++ bin/run.js
@@ -2,9 +2,15 @@
 // ^ we need this env variable above to hide the ExperimentalWarnings
 // source: https://github.com/nodejs/node/issues/10802#issuecomment-573376999
 
+import stringArgv from 'string-argv';
+
 async function main() {
   const { execute } = await import('@oclif/core');
-  await execute({ dir: import.meta.url }).then(msg => {
+  const opts = { dir: import.meta.url };
+  if (process.env.INPUT_RDME) {
+    opts.args = stringArgv(process.env.INPUT_RDME);
+  }
+  await execute(opts).then(msg => {
     if (msg) {
       // eslint-disable-next-line no-console
       console.log(msg);
