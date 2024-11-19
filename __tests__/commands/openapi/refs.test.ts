0c0027e82873a1cc076f7e65ea66e4d432eb950e
admin <admin@cho.local>
2024-11-19 23:50:23 +0100

    test: openapi:refs

---
+++ __tests__/commands/openapi/refs.test.ts
@@ -0,0 +1,74 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+import prompts from 'prompts';
+import { describe, beforeAll, beforeEach, afterEach, it, expect, vi } from 'vitest';
+
+import Command from '../../../src/commands/openapi/refs.js';
+import { runCommand } from '../../helpers/setup-oclif-config.js';
+
+describe('openapi:solving-circularity-recursiveness', () => {
+  let run: (args?: string[]) => Promise<unknown>;
+
+  beforeAll(() => {
+    run = runCommand(Command);
+  });
+
+  beforeEach(() => {
+    vi.spyOn(console, 'info').mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  describe('comparison of OpenAPI files', () => {
+    it('should process circular references', async () => {
+      const inputFile = path.resolve('__tests__/__fixtures__/circular-references-oas/circular-references.json');
+      const expectedOutputFile = path.resolve(
+        '__tests__/__fixtures__/circular-references-oas/circular-references-resolved.json',
+      );
+      const defaultOutputFilePath = 'circular-references.openapi.json';
+
+      prompts.inject([defaultOutputFilePath]);
+
+      let processedOutput;
+      fs.writeFileSync = vi.fn((fileName, data) => {
+        processedOutput = JSON.parse(data as string);
+      });
+
+      const result = await run([inputFile]);
+
+      expect(result).toMatch(`Your API definition has been processed and saved to ${defaultOutputFilePath}!`);
+
+      expect(fs.writeFileSync).toHaveBeenCalledWith(defaultOutputFilePath, expect.any(String));
+
+      const expectedOutput = JSON.parse(fs.readFileSync(expectedOutputFile, 'utf8'));
+      expect(processedOutput).toStrictEqual(expectedOutput);
+    });
+
+    it('should process recursive references', async () => {
+      const inputFile = path.resolve('__tests__/__fixtures__/circular-references-oas/recursive-reference.json');
+      const expectedOutputFile = path.resolve(
+        '__tests__/__fixtures__/circular-references-oas/recursive-reference-resolved.json',
+      );
+      const defaultOutputFilePath = 'recursive-reference.openapi.json';
+
+      prompts.inject([defaultOutputFilePath]);
+
+      let processedOutput;
+      fs.writeFileSync = vi.fn((fileName, data) => {
+        processedOutput = JSON.parse(data as string);
+      });
+
+      const result = await run([inputFile]);
+
+      expect(result).toMatch(`Your API definition has been processed and saved to ${defaultOutputFilePath}!`);
+
+      expect(fs.writeFileSync).toHaveBeenCalledWith(defaultOutputFilePath, expect.any(String));
+
+      const expectedOutput = JSON.parse(fs.readFileSync(expectedOutputFile, 'utf8'));
+      expect(processedOutput).toStrictEqual(expectedOutput);
+    });
+  });
+});
