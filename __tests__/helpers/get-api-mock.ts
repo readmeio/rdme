109ff2308ec2011bd083e808f26f881723b6122d
Kanad Gupta <git@kanad.dev>
2024-12-09 19:11:16 -0600

    test: setup a few test helpers

--- __tests__/helpers/get-api-mock.ts
+++ __tests__/helpers/get-api-mock.ts
@@ -3,6 +3,8 @@ import nock from 'nock';
 import config from '../../src/lib/config.js';
 import { getUserAgent } from '../../src/lib/readmeAPIFetch.js';
 
+import { mockVersion } from './oclif.js';
+
 /**
  * Nock wrapper for ReadMe API v1 that adds required
  * `user-agent` request header so it gets properly picked up by nock.
@@ -19,5 +21,19 @@ export function getAPIv1Mock(reqHeaders = {}) {
 export function getAPIv1MockWithVersionHeader(v: string) {
   return getAPIv1Mock({
     'x-readme-version': v,
+  });
+}
+
+/**
+ * Nock wrapper for ReadMe API v2 that adds required
+ * `user-agent` request header so it gets properly picked up by nock.
+ */
+export function getAPIv2Mock(reqHeaders: nock.Options['reqheaders'] = {}) {
+  return nock(config.host.v2, {
+    reqheaders: {
+      'User-Agent': ua => ua.startsWith(`rdme/${mockVersion}`),
+      'x-readme-source': 'cli',
+      ...reqHeaders,
+    },
   });
 }
