f0008bceb66343478b3b3042dfffdb97f4d1f9b1
Kanad Gupta <git@kanad.dev>
2024-12-17 16:06:00 -0600

    chore: remove unused config opt

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,7 +3,6 @@ const config = {
     v1: 'https://dash.readme.com',
     v2: 'https://api.readme.com/v2',
   },
-  hub: 'https://{project}.readme.io', // this is only used for the `rdme open` command
 } as const;
 
 export default config;
