55bea5ba0e65032b698bf98cdf1a54fcf6d048dc
olehshh <oleh@readme.io>
2025-01-14 20:49:15 +0100

    feat: one more test case

--- __tests__/commands/openapi/refs.test.ts
+++ __tests__/commands/openapi/refs.test.ts
@@ -97,10 +97,36 @@ describe('openapi refs', () => {
       expect(processedOutput).toStrictEqual(expectedOutput);
     });
 
-    it('should process circular references if the schema does not contain properties', async () => {
+    it('should process circular references if the schema contains only $ref', async () => {
       const inputFile = path.resolve('__tests__/__fixtures__/circular-references-oas/circular-reference-ref-only.json');
       const expectedOutputFile = path.resolve(
         '__tests__/__fixtures__/circular-references-oas/circular-reference-ref-only-resolved.json',
+      );
+      const defaultOutputFilePath = 'circular-reference-ref-only.openapi.json';
+
+      prompts.inject([defaultOutputFilePath]);
+
+      let processedOutput;
+      fs.writeFileSync = vi.fn((fileName, data) => {
+        processedOutput = JSON.parse(data as string);
+      });
+
+      const result = await run([inputFile]);
+
+      expect(result).toMatch(`Your API definition has been processed and saved to ${defaultOutputFilePath}!`);
+
+      expect(fs.writeFileSync).toHaveBeenCalledWith(defaultOutputFilePath, expect.any(String));
+
+      const expectedOutput = JSON.parse(fs.readFileSync(expectedOutputFile, 'utf8'));
+      expect(processedOutput).toStrictEqual(expectedOutput);
+    });
+
+    it('should process circular references if the schema contains only $ref with the title', async () => {
+      const inputFile = path.resolve(
+        '__tests__/__fixtures__/circular-references-oas/circular-reference-ref-only-title.json',
+      );
+      const expectedOutputFile = path.resolve(
+        '__tests__/__fixtures__/circular-references-oas/circular-reference-ref-only-title-resolved.json',
       );
       const defaultOutputFilePath = 'circular-reference-ref-only.openapi.json';
 
