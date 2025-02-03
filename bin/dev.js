54b673eaaadf9f894d1e24996eff6f91fb674d20
Kanad Gupta <git@kanad.dev>
2025-02-03 08:36:10 -0600

    chore: add comment

--- bin/dev.js
+++ bin/dev.js
@@ -1,7 +1,15 @@
 #!/usr/bin/env npx tsx
 
 async function main() {
+  /**
+   * This is a v9-specific change to ensure that we're pointing to the
+   * correct npm release channel for version upgrade checks.
+   * Once we deprecate v9, we can remove this line.
+   *
+   * @see {@link https://github.com/oclif/plugin-warn-if-update-available/blob/75634b0cdb6a605b9ecb503576461e60ba9a37b1/README.md?plain=1#L136}
+   */
   process.env.RDME_NEW_VERSION_CHECK_TAG = 'release-v9';
+
   const { execute } = await import('@oclif/core');
   await execute({ development: true, dir: import.meta.url }).then(msg => {
     if (msg && typeof msg === 'string') {
