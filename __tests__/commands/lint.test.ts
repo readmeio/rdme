382e52ea0ae9c96894c688255334fc7d9c600efc
Emily Kuo <em.s.kuo@gmail.com>
2025-02-12 10:31:04 -0800

    test: update test to use `runCommand` instead of `runCommandAndReturnResult`

--- __tests__/commands/lint.test.ts
+++ __tests__/commands/lint.test.ts
@@ -1,20 +1,20 @@
 import { describe, beforeAll, it, expect } from 'vitest';
 
 import Command from '../../src/commands/lint.js';
-import { runCommandAndReturnResult } from '../helpers/oclif.js';
+import { runCommand, type OclifOutput } from '../helpers/oclif.js';
 
 const key = 'rdme_123';
 
 describe('rdme lint', () => {
-  let run: (args?: string[]) => Promise<string>;
+  let run: (args?: string[]) => OclifOutput;
 
   beforeAll(() => {
-    run = runCommandAndReturnResult(Command);
+    run = runCommand(Command);
   });
 
   it('should do something', async () => {
     const results = await run(['__tests__/__fixtures__/docs/new-docs/new-doc.md', '--key', key]);
-    expect(results).toBe('something happens now');
+    expect(results.result).toBe('something happens now');
   });
 });
   
