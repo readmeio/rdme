18936c8d8a18f621dd8e8d0818ef735cfa9df4bc
Emily Kuo <58803587+emilyskuo@users.noreply.github.com>
2025-02-18 15:13:21 -0800

    Update src/commands/openapi/upload.ts
    Co-authored-by: Kanad Gupta <git@kanad.dev>

--- src/commands/openapi/upload.ts
+++ src/commands/openapi/upload.ts
@@ -110,7 +110,7 @@ export default class OpenAPIUploadCommand extends BaseCommand<typeof OpenAPIUplo
     let filename = specFileTypeIsUrl ? nodePath.basename(specPath) : slugify.default(specPath);
 
     if (!specFileTypeIsUrl && filename !== specPath) {
-      this.log(`The slug of your API Definition will be updated to ${filename} to match ReadMe's slug requirements.`);
+      this.log(`The slug of your API Definition will be set to ${filename} in ReadMe. This slug is not visible to your end users. To set this slug to something else, use the \`--slug\` flag.`);
     }
 
     if (this.flags.slug) {
