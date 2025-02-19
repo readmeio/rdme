f1e4d16a40915c5fa4e8d006420cd9d150d082d3
Emily Kuo <58803587+emilyskuo@users.noreply.github.com>
2025-02-19 11:37:53 -0800

    fix(openapi/upload): improved handling for YAML files (#1154)
    | 🚥 Resolves RM-11893, https://github.com/readmeio/rdme/issues/1142 |
    | :------------------- |
    
    ## 🧰 Changes
    
    When uploading an API definition, `rdme` converts all files to JSON,
    which means that YAML files are uploaded in JSON but with the file
    extension of `.yaml` or `.yml`. This will convert files originally in
    YAML back to YAML before uploading to ReadMe.
    
    ## 🧬 QA & Testing
    
    Upload a spec in YAML and confirm in GitHug that it's been stored in
    YAML. Also confirm that JSON specs are uploaded in JSON as well.
    
    ---------
    
    Co-authored-by: Kanad Gupta <git@kanad.dev>

--- src/commands/openapi/upload.ts
+++ src/commands/openapi/upload.ts
@@ -2,6 +2,7 @@ import fs from 'node:fs';
 import nodePath from 'node:path';
 
 import { Flags } from '@oclif/core';
+import yaml from 'js-yaml';
 import ora from 'ora';
 import prompts from 'prompts';
 import slugify from 'slugify';
@@ -115,8 +116,8 @@ export default class OpenAPIUploadCommand extends BaseCommand<typeof OpenAPIUplo
       );
     }
 
+    const fileExtension = nodePath.extname(filename);
     if (this.flags.slug) {
-      const fileExtension = nodePath.extname(filename);
       const slugExtension = nodePath.extname(this.flags.slug);
       if (slugExtension && (!['.json', '.yaml', '.yml'].includes(slugExtension) || fileExtension !== slugExtension)) {
         throw new Error(
@@ -165,11 +166,17 @@ export default class OpenAPIUploadCommand extends BaseCommand<typeof OpenAPIUplo
       this.debug('attaching URL to form data payload');
       body.append('url', specPath);
     } else {
+      const isYaml = fileExtension === '.yaml' || fileExtension === '.yml';
+      // Convert YAML files back to YAML before uploading
+      let specToUpload = preparedSpec;
+      if (isYaml) {
+        specToUpload = yaml.dump(JSON.parse(preparedSpec));
+      }
       // Create a temporary file to write the bundled spec to,
       // which we will then stream into the form data body
-      const { path } = await tmpFile({ prefix: 'rdme-openapi-', postfix: '.json' });
+      const { path } = await tmpFile({ prefix: 'rdme-openapi-', postfix: fileExtension });
       this.debug(`creating temporary file at ${path}`);
-      fs.writeFileSync(path, preparedSpec);
+      fs.writeFileSync(path, specToUpload);
       const stream = fs.createReadStream(path);
 
       this.debug('file and stream created, streaming into form data payload');
@@ -177,7 +184,7 @@ export default class OpenAPIUploadCommand extends BaseCommand<typeof OpenAPIUplo
         [Symbol.toStringTag]: 'File',
         name: filename,
         stream: () => stream,
-        type: 'application/json',
+        type: isYaml ? 'application/x-yaml' : 'application/json',
       });
     }
 
