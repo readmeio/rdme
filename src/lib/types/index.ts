266bc095863e427263435dc48e98221237db740d
Kanad Gupta <git@kanad.dev>
2025-03-07 15:32:57 -0600

    chore: update local openapi snapshot (#1194)
    ## 🧰 Changes
    
    this PR updates our APIv2 openapi description as part of @emilyskuo's
    work in #1175 and chunks out the OAS into its own dedicated file since
    it's absolutely massive. you can check out the diff of the file changes
    [here](https://github.com/readmeio/rdme/compare/next...1346b8b78e5ff852a930ded5f3dc49f722f9cb03).
    
    i tried messing around with JSON module imports so we can eventually
    automate these updates more easily, but we lose out on a lot of good
    strict typing that we currently have with `as const satisfies
    OASDocument`. annoying!
    
    ## 🧬 QA & Testing
    
    no functional changes. do tests still pass?

---
+++ src/lib/types/index.ts
@@ -0,0 +1,26 @@
+import type { FromSchema } from 'json-schema-to-ts';
+
+import readmeAPIv2Oas from './openapiDoc.js';
+
+export const categoryUriRegexPattern =
+  readmeAPIv2Oas.paths['/versions/{version}/guides'].post.requestBody.content['application/json'].schema.properties
+    .category.properties.uri.pattern;
+
+export const parentUriRegexPattern =
+  readmeAPIv2Oas.paths['/versions/{version}/guides'].post.requestBody.content['application/json'].schema.properties
+    .parent.properties.uri.pattern;
+
+type guidesRequestBodySchema =
+  (typeof readmeAPIv2Oas)['paths']['/versions/{version}/guides/{slug}']['patch']['requestBody']['content']['application/json']['schema'];
+
+/**
+ * Derived from our API documentation, this is the schema for the `guides` object
+ * as we send it to the ReadMe API.
+ *
+ * This is only for TypeScript type-checking purposes — we use ajv
+ * to validate the user's schema during runtime.
+ */
+export type GuidesRequestRepresentation = FromSchema<
+  guidesRequestBodySchema,
+  { keepDefaultedPropertiesOptional: true }
+>;
