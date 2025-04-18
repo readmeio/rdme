d2858622bae05bca8e1acb810e4b5232d7cdb8e5
Kanad Gupta <git@kanad.dev>
2025-04-17 22:09:09 -0500

    docs: jsdoc

--- src/lib/hooks/exported.ts
+++ src/lib/hooks/exported.ts
@@ -1,6 +1,9 @@
 import type { PageMetadata } from '../readPage.js';
 import type { Hooks } from '@oclif/core/interfaces';
 
+/**
+ * @note These hooks are under active development for internal usage and are subject to change.
+ */
 export interface PluginHooks extends Hooks {
   pre_markdown_file_scan: {
     options: {
