8254755e11a015ac6cc1c271ed16bc8d2e74210d
Emily Kuo <em.s.kuo@gmail.com>
2025-06-16 16:46:32 -0700

    chore: pr review comments

--- src/lib/types/openapiDoc.ts
+++ src/lib/types/openapiDoc.ts
@@ -6525,7 +6525,7 @@ const document = {
                         description: 'The endpoint HTTP method.',
                       },
                       path: { type: 'string', description: 'The endpoint path.' },
-                      schema: { type: 'string', nullable: true },
+                      schema: { type: 'object', properties: {}, nullable: true },
                       stats: {
                         type: 'object',
                         properties: {
