readmeio/rdme

commit dc414d3d1a7aefd6826e74e2996f1fdc74655cab

refactor: smol type improvement for reference pages (#1268)
## 🧰 Changes

just a small type addition to reflect the new support for reference
pages. we'll want to make a similar change for custom pages + changelog
too.

src/lib/types/index.ts
@@ -10,17 +10,36 @@ export const parentUriRegexPattern =
   readmeAPIv2Oas.paths['/branches/{branch}/guides'].post.requestBody.content['application/json'].schema.properties
     .parent.properties.uri.pattern;
 
+type projectSchema =
+  (typeof readmeAPIv2Oas)['paths']['/projects/me']['get']['responses']['200']['content']['application/json']['schema'];
+
+type apiKeySchema =
+  (typeof readmeAPIv2Oas)['paths']['/projects/{subdomain}/apikeys/{api_key_id}']['get']['responses']['200']['content']['application/json']['schema'];
+
+/** Page schemas */
 type guidesRequestBodySchema =
   (typeof readmeAPIv2Oas)['paths']['/branches/{branch}/guides/{slug}']['patch']['requestBody']['content']['application/json']['schema'];
 
 type guidesResponseBodySchema =
   (typeof readmeAPIv2Oas)['paths']['/branches/{branch}/guides/{slug}']['patch']['responses']['200']['content']['application/json']['schema'];
 
-type projectSchema =
-  (typeof readmeAPIv2Oas)['paths']['/projects/me']['get']['responses']['200']['content']['application/json']['schema'];
+type referenceRequestBodySchema =
+  (typeof readmeAPIv2Oas)['paths']['/branches/{branch}/reference/{slug}']['patch']['requestBody']['content']['application/json']['schema'];
 
-type apiKeySchema =
-  (typeof readmeAPIv2Oas)['paths']['/projects/{subdomain}/apikeys/{api_key_id}']['get']['responses']['200']['content']['application/json']['schema'];
+type referenceResponseBodySchema =
+  (typeof readmeAPIv2Oas)['paths']['/branches/{branch}/reference/{slug}']['patch']['responses']['200']['content']['application/json']['schema'];
+
+/**
+ * Derived from our API documentation, this is the schema for the `project` object
+ * as we receive it to the ReadMe API.
+ */
+export type ProjectRepresentation = FromSchema<projectSchema, { keepDefaultedPropertiesOptional: true }>;
+
+/**
+ * Derived from our API documentation, this is the schema for the API key object
+ * as we receive it to the ReadMe API.
+ */
+export type APIKeyRepresentation = FromSchema<apiKeySchema, { keepDefaultedPropertiesOptional: true }>;
 
 /**
  * Derived from our API documentation, this is the schema for the `guides` object
@@ -44,13 +63,22 @@ export type GuidesResponseRepresentation = FromSchema<
 >;
 
 /**
- * Derived from our API documentation, this is the schema for the `project` object
- * as we receive it to the ReadMe API.
+ * Derived from our API documentation, this is the schema for the `reference` object
+ * as we send it to the ReadMe API.
+ *
+ * This is only for TypeScript type-checking purposes — we use ajv
+ * to validate the user's schema during runtime.
  */
-export type ProjectRepresentation = FromSchema<projectSchema, { keepDefaultedPropertiesOptional: true }>;
+export type ReferenceRequestRepresentation = FromSchema<
+  referenceRequestBodySchema,
+  { keepDefaultedPropertiesOptional: true }
+>;
 
 /**
- * Derived from our API documentation, this is the schema for the API key object
+ * Derived from our API documentation, this is the schema for the `reference` object
  * as we receive it to the ReadMe API.
  */
-export type APIKeyRepresentation = FromSchema<apiKeySchema, { keepDefaultedPropertiesOptional: true }>;
+export type ReferenceResponseRepresentation = FromSchema<
+  referenceResponseBodySchema,
+  { keepDefaultedPropertiesOptional: true }
+>;

src/lib/syncPagePath.ts
@@ -3,6 +3,8 @@ import type {
   GuidesRequestRepresentation,
   GuidesResponseRepresentation,
   ProjectRepresentation,
+  ReferenceRequestRepresentation,
+  ReferenceResponseRepresentation,
 } from './types/index.js';
 
 import chalk from 'chalk';
@@ -15,9 +17,8 @@ import { findPages, type PageMetadata } from './readPage.js';
 import { categoryUriRegexPattern, parentUriRegexPattern } from './types/index.js';
 import { validateFrontmatter } from './validateFrontmatter.js';
 
-// todo: eventually this type will be used for other page types (e.g., API Reference)
-type PageRequestRepresentation = GuidesRequestRepresentation;
-type PageResponseRepresentation = GuidesResponseRepresentation['data'];
+type PageRequestRepresentation = GuidesRequestRepresentation | ReferenceRequestRepresentation;
+type PageResponseRepresentation = GuidesResponseRepresentation['data'] | ReferenceResponseRepresentation['data'];
 
 interface BasePushResult {
   filePath: string;

commit 9e94ac1e1318b05b8bb133829805d431fc3f2558

fix(openapi/upload): handle `pending_update` (#1269)
## 🧰 Changes

we've rolled out background jobs for processing API definition uploads,
but `rdme` failed to account for `pending_update` as a possible API
upload status. this fixes that, and enhances our general type safety a
bit while we're here.

## 🧬 QA & Testing

our CI was previously failing in
https://github.com/readmeio/rdme/pull/1268 due to this issue — is it
passing now?

src/commands/openapi/upload.ts
@@ -1,3 +1,9 @@
+import type {
+  APIUploadSingleResponseRepresentation,
+  APIUploadStatus,
+  StagedAPIUploadResponseRepresentation,
+} from '../../lib/types/index.js';
+
 import nodePath from 'node:path';
 
 import { Flags } from '@oclif/core';
@@ -70,14 +76,19 @@ export default class OpenAPIUploadCommand extends BaseCommand<typeof OpenAPIUplo
     },
   ];
 
+  // eslint-disable-next-line class-methods-use-this
+  private isStatusPending(status: APIUploadStatus): status is 'pending_update' | 'pending' {
+    return status.includes('pending');
+  }
+
   /**
    * Poll the ReadMe API until the upload is complete.
    */
   private async pollAPIUntilUploadIsComplete(slug: string, headers: Headers) {
     let count = 0;
-    let status = 'pending';
+    let status: APIUploadStatus = 'pending';
 
-    while (status === 'pending' && count < 10) {
+    while (this.isStatusPending(status) && count < 10) {
       // eslint-disable-next-line no-await-in-loop, no-loop-func
       await new Promise(resolve => {
         // exponential backoff — wait 1s, 2s, 4s, 8s, 16s, 32s, 30s, 30s, 30s, 30s, etc.
@@ -85,12 +96,15 @@ export default class OpenAPIUploadCommand extends BaseCommand<typeof OpenAPIUplo
       });
       this.debug(`polling API for status of ${slug}, count is ${count}`);
       // eslint-disable-next-line no-await-in-loop
-      const response = await this.readmeAPIFetch(slug, { headers }).then(res => this.handleAPIRes(res));
+      const response = (await this.readmeAPIFetch(slug, { headers }).then(res =>
+        this.handleAPIRes(res),
+      )) as APIUploadSingleResponseRepresentation;
+
       status = response?.data?.upload?.status;
       count += 1;
     }
 
-    if (status === 'pending') {
+    if (this.isStatusPending(status)) {
       throw new Error('Sorry, this upload timed out. Please try again later.');
     }
 
@@ -187,7 +201,7 @@ export default class OpenAPIUploadCommand extends BaseCommand<typeof OpenAPIUplo
       `${method === 'POST' ? 'Creating' : 'Updating'} your API definition to ReadMe...`,
     );
 
-    const response = await this.readmeAPIFetch(
+    const response = (await this.readmeAPIFetch(
       `/branches/${branch}/apis${method === 'POST' ? '' : `/${filename}`}`,
       options,
     )
@@ -195,12 +209,12 @@ export default class OpenAPIUploadCommand extends BaseCommand<typeof OpenAPIUplo
       .catch((err: Error) => {
         spinner.fail();
         throw err;
-      });
+      })) as StagedAPIUploadResponseRepresentation;
 
     if (response?.data?.upload?.status && response?.data?.uri) {
       let status = response.data.upload.status;
 
-      if (status === 'pending') {
+      if (this.isStatusPending(status)) {
         spinner.text = `${spinner.text} uploaded but not yet processed by ReadMe. Polling for completion...`;
         status = await this.pollAPIUntilUploadIsComplete(response.data.uri, headers);
       }

src/lib/types/index.ts
@@ -16,6 +16,12 @@ type projectSchema =
 type apiKeySchema =
   (typeof readmeAPIv2Oas)['paths']['/projects/{subdomain}/apikeys/{api_key_id}']['get']['responses']['200']['content']['application/json']['schema'];
 
+type apiUploadSingleResponseSchema =
+  (typeof readmeAPIv2Oas)['paths']['/branches/{branch}/apis/{filename}']['get']['responses']['200']['content']['application/json']['schema'];
+
+type stagedApiUploadResponseSchema =
+  (typeof readmeAPIv2Oas)['paths']['/branches/{branch}/apis']['post']['responses']['202']['content']['application/json']['schema'];
+
 /** Page schemas */
 type guidesRequestBodySchema =
   (typeof readmeAPIv2Oas)['paths']['/branches/{branch}/guides/{slug}']['patch']['requestBody']['content']['application/json']['schema'];
@@ -31,15 +37,35 @@ type referenceResponseBodySchema =
 
 /**
  * Derived from our API documentation, this is the schema for the `project` object
- * as we receive it to the ReadMe API.
+ * as we receive it from the ReadMe API.
  */
 export type ProjectRepresentation = FromSchema<projectSchema, { keepDefaultedPropertiesOptional: true }>;
 
 /**
  * Derived from our API documentation, this is the schema for the API key object
- * as we receive it to the ReadMe API.
+ * as we receive it from the ReadMe API.
  */
 export type APIKeyRepresentation = FromSchema<apiKeySchema, { keepDefaultedPropertiesOptional: true }>;
+
+/**
+ * Derived from our API documentation, this is the schema for the API upload response
+ * as we receive it from the "Get an API definition" endpoint of the ReadMe API.
+ */
+export type APIUploadSingleResponseRepresentation = FromSchema<
+  apiUploadSingleResponseSchema,
+  { keepDefaultedPropertiesOptional: true }
+>;
+
+/**
+ * Derived from our API documentation, this is the schema for a staged API upload
+ * as we receive it from the `POST` and `PATCH` API definition endpoints of the ReadMe API.
+ */
+export type StagedAPIUploadResponseRepresentation = FromSchema<
+  stagedApiUploadResponseSchema,
+  { keepDefaultedPropertiesOptional: true }
+>;
+
+export type APIUploadStatus = APIUploadSingleResponseRepresentation['data']['upload']['status'];
 
 /**
  * Derived from our API documentation, this is the schema for the `guides` object
@@ -55,7 +81,7 @@ export type GuidesRequestRepresentation = FromSchema<
 
 /**
  * Derived from our API documentation, this is the schema for the `guides` object
- * as we receive it to the ReadMe API.
+ * as we receive from to the ReadMe API.
  */
 export type GuidesResponseRepresentation = FromSchema<
   guidesResponseBodySchema,
@@ -76,7 +102,7 @@ export type ReferenceRequestRepresentation = FromSchema<
 
 /**
  * Derived from our API documentation, this is the schema for the `reference` object
- * as we receive it to the ReadMe API.
+ * as we receive it from the ReadMe API.
  */
 export type ReferenceResponseRepresentation = FromSchema<
   referenceResponseBodySchema,

__tests__/commands/openapi/__snapshots__/upload.test.ts.snap
@@ -176,6 +176,25 @@ exports[`rdme openapi upload > given that the API definition is a local file > a
 }
 `;
 
+exports[`rdme openapi upload > given that the API definition is a local file > and the upload status initially is a pending state > should poll the API until the update is complete 1`] = `
+{
+  "result": {
+    "status": "done",
+    "uri": "/branches/1.0.0/apis/__tests____fixtures__petstore-simple-weird-version.json",
+  },
+  "stderr": "- Validating the API definition located at __tests__/__fixtures__/petstore-simple-weird-version.json...
+ ›   Warning: The slug of your API Definition will be set to 
+ ›   __tests____fixtures__petstore-simple-weird-version.json in ReadMe. This 
+ ›   slug is not visible to your end users. To set this slug to something else,
+ ›    use the \`--slug\` flag.
+- Updating your API definition to ReadMe...
+✔ Updating your API definition to ReadMe... uploaded but not yet processed by ReadMe. Polling for completion... done!
+",
+  "stdout": "🚀 Your API definition (__tests____fixtures__petstore-simple-weird-version.json) was successfully updated in ReadMe!
+",
+}
+`;
+
 exports[`rdme openapi upload > given that the API definition is a local file > and the upload status initially is a pending state > should poll the API until the upload is complete 1`] = `
 {
   "result": {

__tests__/commands/openapi/upload.test.ts
@@ -238,6 +238,44 @@ describe('rdme openapi upload', () => {
         mock.done();
       });
 
+      it('should poll the API until the update is complete', async () => {
+        prompts.inject([true]);
+
+        const mock = getAPIv2Mock({ authorization: `Bearer ${key}` })
+          .get(`/branches/${branch}/apis`)
+          .reply(200, { data: [{ filename: slugifiedFilename }] })
+          .put(`/branches/1.0.0/apis/${slugifiedFilename}`, body =>
+            body.match(`form-data; name="schema"; filename="${slugifiedFilename}"`),
+          )
+          .reply(200, {
+            data: {
+              upload: { status: 'pending_update' },
+              uri: `/branches/${branch}/apis/${slugifiedFilename}`,
+            },
+          })
+          .get(`/branches/${branch}/apis/${slugifiedFilename}`)
+          .times(4)
+          .reply(200, {
+            data: {
+              upload: { status: 'pending_update' },
+              uri: `/branches/${branch}/apis/${slugifiedFilename}`,
+            },
+          })
+          .get(`/branches/${branch}/apis/${slugifiedFilename}`)
+          .reply(200, {
+            data: {
+              upload: { status: 'done' },
+              uri: `/branches/${branch}/apis/${slugifiedFilename}`,
+            },
+          });
+
+        const result = await run(['--branch', branch, filename, '--key', key]);
+
+        expect(result).toMatchSnapshot();
+
+        mock.done();
+      });
+
       it('should poll the API and handle timeouts', async () => {
         const mock = getAPIv2Mock({ authorization: `Bearer ${key}` })
           .get(`/branches/${branch}/apis`)