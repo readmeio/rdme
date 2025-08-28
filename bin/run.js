f73171369a9628a7ad7f83550e8955249a8cfdbf
Kanad Gupta <git@kanad.dev>
2025-08-27 21:35:20 -0500

    fix(oclif): disable engine warning (#1333)
    | 🚥 Resolves https://github.com/readmeio/rdme/issues/1331 |
    | :------------------- |
    
    ## 🧰 Changes
    
    as first discovered in https://github.com/readmeio/rdme/issues/1331,
    [`@oclif/core` uses **_its own_** node engine requirements and emits a
    warning](https://github.com/oclif/core/blob/70d3f192862a5adb548cbda68c6ee1ca8f724110/src/index.ts#L12-L27)
    if its node version requirements aren't met:
    
    they require v18 or later but we require v20 or later, so the warning
    looks like this (which is wrong):
    
    ```
    (node:4619) Warning: Node version must be >=18.0.0 to use this CLI. Current node version: 14.20.0
    ```
    
    due to this being misleading, we're going to disable this warning
    entirely. i don't **_think_** we need to emit warnings for mismatched
    versions of node.js (since the warning is already emitted when users
    install this CLI via `npm`) but i'm open to reevaluating that.
    
    ## 🧬 QA & Testing
    
    confirmed that using a version of node.js less than 18 no longer emits
    this warning.

--- bin/run.js
+++ bin/run.js
@@ -3,6 +3,13 @@
 import stringArgv from 'string-argv';
 
 async function main() {
+  /**
+   * Disables the oclif engine warning. For some reason the versions that are flagged are totally separate from our engines requirements.
+   *
+   * @see {@link https://github.com/oclif/core/blob/70d3f192862a5adb548cbda68c6ee1ca8f724110/src/index.ts#L12}
+   */
+  process.env.OCLIF_DISABLE_ENGINE_WARNING = 'true';
+
   const { execute } = await import('@oclif/core');
   const opts = { dir: import.meta.url };
   if (process.env.INPUT_RDME) {
